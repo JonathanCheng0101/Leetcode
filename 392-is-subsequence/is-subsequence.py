1e3f21e9ae50201216839eaac694f0e0d2d549b0
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2025-05-18 10:57:17 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (41.95%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,20 +1,23 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        # empty s is always a subsequence
         if not s:
             return True
-        # if s is longer than t it can't be
+        # edge case s > t return false
         if len(s) > len(t):
             return False
 
         i = 0
         for c in t:
-            # first check that i is still in‐bounds, then compare
-            if i < len(s) and s[i] == c:
+            if s[i] == c and i < len(s):
                 i += 1
-                # early exit once we've matched all of s
+
                 if i == len(s):
                     return True
+                  
 
-        # if we've advanced i through all of s, it's a subsequence
         return i == len(s)
+
+
+        
+
+        
