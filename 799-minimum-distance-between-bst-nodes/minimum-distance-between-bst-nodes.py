73a1cbed1e0e77c28b278a45673a42ab3f1864e4
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2025-05-22 20:14:00 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (21.12%) - LeetSync

---
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        res = []
+        q = deque([root])
+
+        while q:
+            length = len(q)
+
+            for _ in range(length):
+                node = q.popleft()
+                res.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+
+                if node.right:
+                    q.append(node.right)
+        
+        res.sort()
+        min_diff = float('inf')
+        for i in range(1, len(res)):
+            curr_diff = res[i] - res[i - 1]
+            if curr_diff < min_diff:
+                min_diff = curr_diff
+        
+        return min_diff
+
+        
