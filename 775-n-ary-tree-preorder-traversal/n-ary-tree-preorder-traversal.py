772988ad504f93695bc342bc2d3d2f9945983711
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2025-05-27 20:38:31 -0600

    Time: 47 ms (69.83%) | Memory: 19.4 MB (46.91%) - LeetSync

--- 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
@@ -10,15 +10,18 @@ class Solution:
     def preorder(self, root: 'Node') -> List[int]:
         res = []
 
+        # now --> left--> right
         def dfs(node):
             if not node:
                 return 
-            
+
             res.append(node.val)
             for child in node.children:
                 dfs(child)
-
+                
         dfs(root)
 
         return res
-        
+
+            
+
