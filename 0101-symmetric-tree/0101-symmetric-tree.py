e0ef5f22c6cbdc0477c0a36bf0e3ff7a86e2d06d
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2025-06-01 12:14:28 -0600

    Time: 0 ms (100%), Space: 18 MB (12.66%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,24 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        
-        def dfs(node,lst,chk):
-            if not node:
-                lst.append(None)
-                return 
-            lst.append(node.val)
-            if chk== 1:
-                dfs(node.left, lst, chk)
-                dfs(node.right, lst, chk)
-            else:
-                dfs(node.right, lst, chk)
-                dfs(node.left, lst, chk)
+        res1, res2 = [], []
 
+        def dfs(node,res,isLeft):
+            if not node:
+                res.append(None)
+                return 
+            res.append(node.val)
+            
+            if isLeft:
+                dfs(node.left, res, isLeft)
+                dfs(node.right, res, isLeft)
+            else:
+                dfs(node.right, res, isLeft)
+                dfs(node.left, res, isLeft)
+
+        dfs(root.left, res1, True)
+        dfs(root.right, res2, False)
+        return res1 == res2
 
             
-        res1, res2 = [], []
-        dfs(root.left, res1, 1)
-        dfs(root.right, res2, 0)
-        return res1 == res2
+
+            
+        
