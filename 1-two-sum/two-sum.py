8efc23daa8fe96527a0087eae5a1da375c490e5b
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-14 17:11:43 -0600

    Time: 0 ms (100.00%) | Memory: 20.6 MB (25.47%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,17 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         res = {}
-        for i, ele in enumerate(nums):
-            need = target - ele
+        for i,ele in enumerate(nums):
+            need = target - nums[i]
             if need in res:
-                return [i, res[need]]
+                return [i,res[need]]
             else:
-                res[ele] = i 
-
-
+                res[nums[i]] = i
             
         
-
+"""
+我將問題轉換為在遍歷過程中查找補數， 並使用 HashMap 將查詢時間從 O(n) 降到 O(1)
+"""
 
 
             
