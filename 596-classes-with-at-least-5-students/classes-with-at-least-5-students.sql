0d6ff69b9140f44c67cf11f5bcff979e34116a71
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-15 17:05:44 -0600

    Time: 361 ms (15.63%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,8 +1,5 @@
 # Write your MySQL query statement below
-SELECT class 
-FROM Courses
+SELECT class
+FROM courses
 GROUP BY class
-HAVING count(student)>=5
-
-
-#COUNT = 幾筆資料（幾個人） SUM = 數值加總
+HAVING COUNT(*) >= 5
