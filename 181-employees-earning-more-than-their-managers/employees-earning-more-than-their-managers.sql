910ffc461a99e694bfb0c0ac751e3f01765b2aed
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-15 16:00:30 -0600

    Time: 391 ms (32.89%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,12 +1,6 @@
 # Write your MySQL query statement below
-SELECT e.name AS Employee
-FROM Employee e
-JOIN Employee m
-on e.managerID = m.id
-WHERE e.salary > m.salary;
-
-
-#ON 是告訴資料庫兩張表要怎麼配對。
-#JOIN 只是說「我要合併」，但資料庫不知道哪一行要對哪一行，所以用 ON 給出規則：
-#sqlJOIN Employee m
-#ON e.managerId = m.id
+SELECT e1.name AS Employee
+FROM Employee e1
+LEFT JOIN Employee e2
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary
