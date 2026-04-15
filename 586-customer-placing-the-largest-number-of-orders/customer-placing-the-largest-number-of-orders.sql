66e606e3105b74445203a0ed7a1eb02ecde55f00
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-15 16:58:03 -0600

    Time: 484 ms (23.65%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,12 +1,6 @@
 # Write your MySQL query statement below
-# COUNT(*)
 SELECT customer_number
 FROM Orders
 GROUP BY customer_number
-ORDER BY COUNT(*) DESC
-LIMIT 1 
-
-# SELECT 後面接啥 就會顯示啥
-# sum是對裡面的值相加 count就是單純算幾次
-# ORDER BY ... DESC   -- 由大到小（最大在前）
-# ORDER BY ... ASC    -- 由小到大（最小在前，預設）
+ORDER BY count(*) DESC
+LIMIT 1; 
