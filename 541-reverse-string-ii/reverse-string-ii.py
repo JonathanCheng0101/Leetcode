bea9ed813756064c419cb2df023eb0e9db795ff7
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-18 17:18:07 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (45.04%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -1,22 +1,9 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         res = []
-        temp = []
-        word = list(s)
-        for i, char in enumerate(word):
-            if i % (2 * k) < k:
-                temp.append(char)
-            else:
-                if temp:
-                    new_temp = temp[::-1]
-                    for i in new_temp:
-                        res.append(i)
-                    temp = []
-                
-                res.append(char)
-        if temp:
-            res.extend(temp[::-1])
 
-        ans = "".join(res)
-        return ans
-            
+        for i in range(0, len(s), 2 * k):
+            res.append(s[i:i + k][::-1])
+            res.append(s[i + k:i + 2 * k])
+
+        return "".join(res)
