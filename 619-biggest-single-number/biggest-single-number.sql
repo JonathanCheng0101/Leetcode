7d078da5ab771cc4302837de2b4a88f1b689a7a6
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-18 10:09:47 -0600

    Time: 403 ms (67.42%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,21 +1,7 @@
-# Write your MySQL query statement below
-SELECT MAX(num) AS num
-FROM(
+SELECT Max(num) AS num
+FROM (
     SELECT num
     FROM MyNumbers
     GROUP BY num
-    HAVING COUNT(*) = 1) t
-
-
-# 為何一定要有 t 因為只要 只要你在 FROM 裡面放一個子查詢
-# FROM ( ... )
-#就 一定要幫它取名字
-
-#WHERE 用來篩選「row」（分組前）
-#HAVING 用來篩選「group」（分組後）
-# 核心差別（超重要）
-#	                    WHERE	       HAVING
-#時機	              GROUP BY 前	GROUP BY 後
-#篩選對象               單筆 row	  一整個 group
-#能不能用COUNT / SUM	❌ 不行       ✅ 可以
-
+    HAVING COUNT(num) = 1
+) t;
