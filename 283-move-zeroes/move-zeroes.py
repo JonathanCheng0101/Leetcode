1b06871b27619f8a0a0f3952fcd0bfac5c889286
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-19 17:43:36 -0600

    Time: 3 ms (82.62%) | Memory: 20.3 MB (98.39%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,16 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # 若都不是0相安無事,碰到0 slow stays, fast goes
-        l = 0
-        for r in range(len(nums)):
-            if nums[r]: # none zero 的時候進行swap，l == r的時候swap也沒差
-                nums[l], nums[r] = nums[r], nums[l]
+        # two pointers
+        slow = 0
 
-                l += 1        
-        
-            
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
 
-            
-
-        
+                slow += 1
