b37bc5ac11e08eb67296371f2dc0be75c6905da9
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-19 20:36:29 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (48.49%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,28 +1,8 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        res = []
-        curr = ""
+        s = s.strip()
+        s_lst = s.split()
+    
 
-        for char in s:
-            if not char.isalpha() and not char.isalnum():
-                if len(curr) == 0:
-                    continue
-                else:
-                    res.append(curr)
-                    curr = ""
-
-            else:
-                curr += char
-
-        if curr:
-            res.append(curr)
-            
-        # return " ".join(res[:: -1])
-        result = ""
-        for i in range(len(res)-1, -1, -1):
-            result += res[i]
-            result += " "
-
-        return result[:-1]
-
-            
+        new_s = s_lst[::-1]
+        return " ".join(new_s)
