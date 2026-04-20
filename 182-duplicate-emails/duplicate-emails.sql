14f0a5a6d0b8ef3382a778b161e3989d590c92c1
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-20 14:32:22 -0600

    Time: 368 ms (50.65%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below
-SELECT email  # 最後要呈現的東西
-FROM Person 
-GROUP BY email   # 把資料「按照某個欄位分組」
-HAVING  COUNT(*)> 1 # 對「分組結果」做條件篩選
+SELECT DISTINCT email AS Email
+FROM Person
+GROUP BY email
+HAVING COUNT(*) > 1
