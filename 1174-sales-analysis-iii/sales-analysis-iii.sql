c29f77f15174d630457a1d24fa3ab6f299da94b0
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-22 16:53:09 -0600

    Time: 1121 ms (54.57%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -3,10 +3,6 @@ SELECT DISTINCT p.product_id, p.product_name
 FROM Product p
 JOIN Sales s
 ON p.product_id = s.product_id
-WHERE p.product_id NOT IN (
-    SELECT product_id
-    FROM Sales
-    WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'
-);
-
-
+GROUP BY p.product_id
+HAVING MIN(sale_date) >= '2019-01-01' AND 
+       MAX(sale_date) <= '2019-03-31';
