c81f5c40e0411a26304a2288443035cbe5580632
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-24 08:57:04 -0600

    Time: 563 ms (32.52%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,9 +1,5 @@
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'OR 
-       employee_id NOT IN(
-        SELECT employee_id
-        FROM Employee
-        WHERE primary_flag = 'Y'
-);
-      
+SELECT e1.employee_id, e1.department_id
+FROM Employee e1
+LEFT JOIN Employee e2
+ON e1.employee_id = e2.employee_id AND e2.primary_flag = 'Y'
+WHERE e1.primary_flag = 'Y' OR e2.primary_flag IS NULL;
