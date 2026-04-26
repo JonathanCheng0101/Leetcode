19b5d65a6f8cb9e8ad31ac16bad987093236271c
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-04-26 11:06:06 -0600

    Time: 493 ms (11.17%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,9 +1,8 @@
-SELECT p.firstName,
-       p.lastName,
+# Write your MySQL query statement below
+SELECT p.firstname, 
+       p.lastname,
        a.city,
        a.state
 FROM Person p
 LEFT JOIN Address a
-ON p.personId = a.personId
-
-       
+ON p.personId = a.personId;
