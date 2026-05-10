21da52e8c49ef77066956802964901b75c5a8c14
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-10 16:09:23 -0600

    Time: 418 ms (19.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT DISTINCT c1.seat_id
+FROM Cinema c1
+JOIN Cinema c2
+ON abs(c1.seat_id - c2.seat_id) = 1
+WHERE c1.free = 1 AND c2.free = 1
+ORDER by c1.seat_id ASC;
