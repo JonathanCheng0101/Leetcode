56358addcbcfba9c44c68f0cc5111f8a660e00f0
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-11 23:42:55 -0600

    Time: 967 ms (41.24%) | Memory: 0B (100.00%) - LeetSync

--- 1639-friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
+++ 1639-friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
 SELECT DISTINCT c.title
 FROM TVProgram t
 JOIN Content c
 ON t.content_id = c.content_id
-WHERE DATE_FORMAT(t.program_date, "%Y-%m") = '2020-06' AND c.Kids_content = "Y" AND c.content_type = "Movies";
+WHERE DATE_FORMAT(t.program_date,'%Y-%m') = '2020-06' AND c.Kids_content = "Y" AND c.content_type = "Movies";
