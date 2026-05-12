2830e8c88bca03de03192dadea19f7f4d29e8f93
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-11 23:34:35 -0600

    Time: 434 ms (49.16%) | Memory: 0B (100.00%) - LeetSync

--- 1564-create-a-session-bar-chart/create-a-session-bar-chart.sql
+++ 1564-create-a-session-bar-chart/create-a-session-bar-chart.sql
@@ -1,23 +1,24 @@
-# Write your MySQL query statement below
-WITH original AS(
-  SELECT '[0-5>' AS bin
-          UNION ALL
-          SELECT"[5-10>"
-          UNION ALL
-          SELECT "[10-15>"
-          UNION ALL
-          SELECT "15 or more"
-    ),
-t AS(
-  SELECT CASE WHEN duration < 300 THEN '[0-5>' 
-               WHEN duration BETWEEN 300 AND 599 THEN "[5-10>" 
-               WHEN duration BETWEEN 600 AND 899 THEN "[10-15>" 
-               WHEN duration  >= 900 THEN "15 or more"  END AS bin
-  FROM Sessions
+WITH t AS(
+    SELECT *,
+           CASE WHEN duration < 300 THEN '[0-5>'
+                WHEN duration BETWEEN 300 AND 599 THEN '[5-10>'
+                WHEN duration BETWEEN 600 AND 899 THEN '[10-15>'
+                WHEN duration > 900 THEN '15 or more' END AS `bin`
+    FROM Sessions
 )
-SELECT o.bin,
-       IFNULL(COUNT(t.bin),0) AS total
-FROM original o
+, p AS(
+    SELECT '[0-5>' AS bin
+    UNION ALL
+    SELECT '[5-10>'
+    UNION ALL
+    SELECT '[10-15>'
+    UNION ALL
+    SELECT '15 or more'
+)
+SELECT p.`bin`,
+       COUNT(t.`bin`) AS total
+FROM p
 LEFT JOIN t
-ON o.bin = t.bin
-GROUP BY o.bin;
+ON p.`bin` = t.`bin`
+GROUP BY p.`bin`
+
