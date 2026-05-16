75f8ca4f0f36c9a053a8cd5f13ab16995b007eaa
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-16 11:33:56 -0600

    Time: 534 ms (41.98%) | Memory: 0B (100.00%) - LeetSync

--- 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
+++ 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -1,12 +1,10 @@
-# Write your MySQL query statement below
 WITH f AS(
-    SELECT COUNT(DISTINCT sender_id, send_to_id) AS f_cnt
+    SELECT COUNT(DISTINCT sender_id, send_to_id) AS r_cnt
     FROM FriendRequest
 ),
-r AS (
-    SELECT COUNT(DISTINCT requester_id, accepter_id) AS r_cnt
+r AS(
+    SELECT COUNT(DISTINCT requester_id, accepter_id) AS a_cnt
     FROM RequestAccepted
 )
-SELECT ROUND(IFNULL((r.r_cnt/ f.f_cnt),0.00),2) AS accept_rate
-FROM f
-JOIN r
+SELECT ROUND(IFNULL((r.a_cnt/f.r_cnt),0),2) AS accept_rate
+FROM f,r
