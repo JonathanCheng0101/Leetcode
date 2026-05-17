b9eb48cc8f622fbb8acf4d4b450b5be8a28abf9a
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-17 10:07:47 -0600

    Time: 308 ms (33.57%) | Memory: 0B (100.00%) - LeetSync

--- 3284-find-expensive-cities/find-expensive-cities.sql
+++ 3284-find-expensive-cities/find-expensive-cities.sql
@@ -1,10 +1,14 @@
 # Write your MySQL query statement below
 WITH t AS(
-    SELECT AVG(price) AS nl_avg
-    FROM Listings
-)
-SELECT l.city
-FROM Listings l,t
-GROUP BY city
-HAVING AVG(l.price) > MAX(t.nl_avg)
-ORDER BY l.city;
+    SELECT *,
+          (AVG(price) OVER(PARTITION BY city)) AS city_avg
+    FROM Listings),
+    n AS(
+    SELECT *,
+          (AVG(price)) AS national_avg
+    FROM Listings)
+
+SELECT DISTINCT t.city
+FROM t,n
+WHERE t.city_avg > n.national_avg
+ORDER BY t.city ASC;
