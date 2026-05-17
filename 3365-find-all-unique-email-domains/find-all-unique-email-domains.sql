4f469b9b6ce6b1d9242732f729b1348d1c8f2102
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-17 10:21:47 -0600

    Time: 321 ms (26.80%) | Memory: 0B (100.00%) - LeetSync

--- 3365-find-all-unique-email-domains/find-all-unique-email-domains.sql
+++ 3365-find-all-unique-email-domains/find-all-unique-email-domains.sql
@@ -1,7 +1,6 @@
-# Write your MySQL query statement below
-SELECT SUBSTRING_INDEX(email, '@', -1) AS email_domain,
+SELECT SUBSTRING_INDEX(email,'@',-1) AS email_domain,
        COUNT(*) AS `count`
 FROM Emails
 WHERE email REGEXP '\\.com$'
-GROUP BY email_domain
+GROUP BY SUBSTRING_INDEX(email,'@',-1)
 ORDER BY email_domain ASC;
