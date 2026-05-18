257a95221864e3097f15e457bdfc5991fc5f12fa
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-18 17:22:27 -0600

    Time: 540 ms (82.91%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,13 +1,9 @@
-# Write your MySQL query statement below
 WITH t AS(
-    SELECT COUNT(*) OVER(PARTITION BY tiv_2015) AS cnt_2015,
-           COUNT(*) OVER(PARTITION BY lat,lon) AS location,
-           tiv_2016
+    SELECT tiv_2016,
+           COUNT(*) OVER(PARTITION BY tiv_2015) AS cnt_2015,
+           COUNT(*) OVER(PARTITION BY lat,lon) AS latlon_cnt
     FROM Insurance
 )
-
-
-
-SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016
 FROM t
-WHERE cnt_2015 > 1 AND location = 1;
+WHERE cnt_2015 > 1 AND latlon_cnt = 1
