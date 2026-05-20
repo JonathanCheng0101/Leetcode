fa197e9959c87ff7d514d45afa4d127b796fa738
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-20 14:57:52 -0600

    Time: 471 ms (66.49%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,18 +1,17 @@
-# Write your MySQL query statement below
 WITH t AS(
-    SELECT *,
-           ROW_NUMBER() OVER (
-                     PARTITION BY product_id 
-                     ORDER BY change_date DESC) AS rn
+    SELECT DISTINCT product_id,
+           10 AS price
     FROM Products
-    WHERE change_date <= '2019-08-16'
 ),
 a AS(
-    SELECT DISTINCT product_id
+    SELECT product_id,
+           new_price,
+           ROW_NUMBER()OVER(PARTITION BY product_id ORDER BY change_date DESC) AS rn
     FROM Products
+    WHERE change_date <= '2019-08-16'
 )
-SELECT a.product_id,
-       IFNULL(t.new_price,10) AS price
-FROM a
-LEFT JOIN t
-ON a.product_id = t.product_id AND t.rn = 1
+SELECT t.product_id,
+       IFNULL(a.new_price, t.price) AS price
+FROM t
+LEFt JOIN a
+ON t.product_id = a.product_id AND rn = 1
