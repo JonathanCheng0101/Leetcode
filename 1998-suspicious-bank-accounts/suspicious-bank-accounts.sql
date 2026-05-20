b8f662868ed46b193ab211479a7504a452ce39cd
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-20 16:13:45 -0600

    Time: 1024 ms (57.03%) | Memory: 0B (100.00%) - LeetSync

--- 1998-suspicious-bank-accounts/suspicious-bank-accounts.sql
+++ 1998-suspicious-bank-accounts/suspicious-bank-accounts.sql
@@ -1,31 +1,21 @@
 # Write your MySQL query statement below
 WITH t AS(
     SELECT t.account_id,
-            DATE_FORMAT(t.`day`, "%Y-%m") AS `month`,
-          (CASE WHEN SUM(t.amount)> a.max_income THEN 1 ELSE 0 END) AS sus
+       DATE_FORMAT(t.`day`, "%Y-%m")AS `month`,
+       CASE WHEN SUM(t.amount) > (a.max_income) THEN 1 ELSE 0 END AS over_limit
     FROM Transactions t
     JOIN Accounts a
     ON t.account_id = a.account_id
-    WHERE t.type = "Creditor"
-    GROUP BY t.account_id, `month`
-),
-t2 AS (
-    SELECT 
-        account_id,
-        `month`,
-        CASE 
-            WHEN (
-                LAG(sus) OVER(PARTITION BY account_id ORDER BY `month`) + sus = 2
-                AND TIMESTAMPDIFF(
-                    MONTH,
-                    STR_TO_DATE(CONCAT(LAG(`month`) OVER(PARTITION BY account_id ORDER BY `month`), '-01'), '%Y-%m-%d'),
-                    STR_TO_DATE(CONCAT(`month`, '-01'), '%Y-%m-%d')
-                ) = 1
-            )
-            THEN 1
-        END AS prev_month_sus
+    WHERE t.type = 'Creditor'
+    GROUP BY t.account_id, DATE_FORMAT(t.`day`, "%Y-%m")
+), a AS(
+    SELECT account_id,
+       CASE WHEN TIMESTAMPDIFF(month,LAG(CONCAT(`month`,"-01"))OVER(PARTITION BY account_id ORDER BY `month` ASC),CONCAT(`month`,"-01")) = 1 THEN 1 ELSE 0 END AS continuous_month 
     FROM t
+    WHERE over_limit = 1
 )
 SELECT DISTINCT account_id
-FROM t2
-WHERE prev_month_sus = 1;
+FROM a
+WHERE continuous_month = 1;
+
+
