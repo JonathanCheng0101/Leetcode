255b27d34437489087f632303481dc3531847cf1
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-20 15:45:55 -0600

    Time: 644 ms (21.57%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
 WITH t AS(
-    SELECT *, MIN(event_date) OVER(PARTITION BY player_id ORDER BY event_date ASC) AS first_date
+    SELECT *,
+           MIN(event_date)OVER(PARTITION BY player_id) AS first_date
     FROM Activity
 )
-SELECT IFNULL(ROUND (SUM(CASE WHEN DATEDIFF(event_date, first_date) = 1 THEN 1 END)/COUNT(DISTINCT player_id),2), 0) AS fraction
-FROM t
+SELECT ROUND(SUM(CASE WHEN DATEDIFF(event_date,first_date) = 1 THEN 1 ELSE 0 END)/COUNT(DISTINCT player_id),2) AS fraction
+FROM t;
