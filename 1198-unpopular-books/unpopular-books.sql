377eb5d8f1efdd64eed8b64c8cbbc44c63e2c547
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-21 15:06:49 -0600

    Time: 982 ms (9.37%) | Memory: 0B (100.00%) - LeetSync

--- 1198-unpopular-books/unpopular-books.sql
+++ 1198-unpopular-books/unpopular-books.sql
@@ -1,18 +1,19 @@
-# Write your MySQL query statement below
 WITH t AS(
-    SELECT book_id,
-           name
+    SELECT *
     FROM Books
-    WHERE TIMESTAMPDIFF(day, available_from, '2019-06-23') > 31
+    WHERE TIMESTAMPDIFF(day, available_from, '2019-06-23') >= 30
 ), o AS(
-    SELECT book_id,
-           quantity
+    SELECT book_id, quantity
     FROM Orders
     WHERE TIMESTAMPDIFF(day, dispatch_date, '2019-06-23') <= 365
+    GROUP BY book_id
+    HAVING SUM(quantity) >= 10
 )
-SELECT t.book_id, t.name
+SELECT t.book_id,
+       t.name
 FROM t
-LEFT JOIN o
-ON t.book_id = o.book_id
-GROUP BY t.book_id
-HAVING IFNULL(SUM(o.quantity),0) < 10;
+WHERE NOT EXISTS(
+    SELECT 1
+    FROM o
+    WHERE t.book_id = o.book_id
+)
