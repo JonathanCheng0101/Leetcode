12164ac4eaac4fe2a61965a3d5ed66eda60db6b9
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-21 16:23:47 -0600

    Time: 1349 ms (28.46%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
-
-SELECT DISTINCT u.user_id AS buyer_id,
+SELECT  u.user_id AS buyer_id,
        u.join_date,
-       IFNULL(COUNT(o.item_id)OVER (PARTITION BY o.buyer_id), 0) AS orders_in_2019
+       COUNT(o.item_id) AS orders_in_2019
 FROM Users u
 LEFT JOIN Orders o
-ON u.user_id = o.buyer_id AND DATE_FORMAT(o.order_date,'%Y') = '2019'
+ON u.user_id = o.buyer_id AND DATE_FORMAT(o.order_date, "%Y") = '2019' 
+GROUP BY u.user_id, u.join_date
+
 
