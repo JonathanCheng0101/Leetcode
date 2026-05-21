44fc2a2b5eca7dfc96aed8546c861d16d3dd5c91
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-21 14:21:13 -0600

    Time: 359 ms (42.95%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,10 +1,4 @@
-# Write your MySQL query statement below
-WITH t AS(
-    SELECT id,
-       CASE WHEN id % 2 = 1 THEN IFNULL(LEAD(student)OVER(ORDER BY id),student) 
-            WHEN id % 2 = 0 THEN LAG(student)OVER(ORDER BY id) END AS new_student
-    FROM Seat
-)
 SELECT id,
-       new_student AS student
-FROM t
+       IFNULL((CASE WHEN id % 2 = 1 THEN LEAD(student) OVER(ORDER BY id ASC)
+            WHEN id % 2 = 0 THEN LAG(student) OVER(ORDER BY id ASC) END), student) AS student
+FROM Seat;
