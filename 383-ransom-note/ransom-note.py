f82e7aadd95f764a75e94a174982832aa8343707
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-21 10:22:08 -0600

    Time: 14 ms (80.02%) | Memory: 19.6 MB (72.43%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,22 +1,6 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         # r 是 m 的子集合
-        r_dict = defaultdict(int)
-        m_dict = defaultdict(int)
-
-        for ele in ransomNote:
-            r_dict[ele] += 1
-
-        for ele in magazine:
-            m_dict[ele] += 1
-
-        for ele in r_dict:
-            if ele not in m_dict:
-                return False
-            else:
-                m_dict[ele] -= r_dict[ele]
-        for ele in m_dict:
-            if m_dict[ele] < 0:
-                return False
-        return True
+        return not (Counter(ransomNote) - Counter(magazine))
+    
