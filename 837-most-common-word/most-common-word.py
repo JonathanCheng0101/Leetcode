3036de25adab41ec8085d42ee06b57701f039f80
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-23 09:25:11 -0600

    Time: 3 ms (62.41%) | Memory: 19.6 MB (5.38%) - LeetSync

--- 837-most-common-word/most-common-word.py
+++ 837-most-common-word/most-common-word.py
@@ -4,10 +4,5 @@ from collections import Counter
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         wrds = re.findall(r"[a-z]+", paragraph.lower())
-        sorted_wrd = sorted(Counter(wrds).items(), key = lambda x: x[1], reverse = True)
-        
-        for k, _ in sorted_wrd:
-            if k in set(banned):
-                continue
-            else:
-                return k
+        good_wrd = [wrd for wrd in wrds if wrd not in set(banned)]
+        return Counter(good_wrd).most_common(1)[0][0]
