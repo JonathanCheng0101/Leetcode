e79f3c824b54f52edc4da7f80238150068c6f4d9
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-24 16:52:51 -0600

    Time: 558 ms (58.04%) | Memory: 0B (100.00%) - LeetSync

--- 1399-page-recommendations/page-recommendations.sql
+++ 1399-page-recommendations/page-recommendations.sql
@@ -1,16 +1,19 @@
 # Write your MySQL query statement below
-WITH t AS(
-    SELECT l2.user_id,
-       l2.page_id
-    FROM Likes l2
+WITH f AS(
+    SELECT CASE WHEN user1_id = 1 THEN user2_id ELSE user1_id END AS friend
+    FROM Friendship 
+    WHERE user1_id = 1 OR user2_id = 1
+), l AS(
+    SELECT l1.user_id, 
+           l1.page_id
+    FROM Likes l1
     WHERE NOT EXISTS(
-        SELECT 1 
-        FROM Likes l1
-        WHERE l1.user_id = 1 AND l1.page_id = l2.page_id 
+        SELECT 1
+        FROM Likes l2
+        WHERE l2.user_id = 1 AND l2.page_id = l1.page_id
     )
 )
-SELECT DISTINCT t.page_id AS recommended_page
-FROM Friendship f
-JOIN t
-ON GREATEST(f.user1_id, f.user2_id) = t.user_id
-WHERE LEAST(user1_id, user2_id) = 1
+SELECT DISTINCT l.page_id AS recommended_page
+FROM l
+JOIN f
+ON f.friend = l.user_id
