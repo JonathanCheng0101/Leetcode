ad50de5e300765d898b6d695de4a4f627fcf6225
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-26 15:06:40 -0600

    Time: 191 ms (21.71%) | Memory: 19.8 MB (69.09%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,26 +1,26 @@
+from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(p) > len(s):
-            return []
         res = []
+        k= len(p)
+        cnt_p = Counter(p)
+        window = Counter(s[:k])
+        if window == cnt_p:
+                res.append(0)
 
-        checker = [0] * 26
-        cur_window = [0] * 26
-        a = ord('a')
+        for right in range(k, len(s)):
+            window[s[right]] += 1
 
-        # check the first one
-        for i in range(len(p)):
-            checker[ord(p[i]) - a] += 1
-            cur_window[ord(s[i]) - a] += 1
+            left = right - k
+            window[s[left]] -= 1
 
-        if checker == cur_window:
-            res.append(0)
+            if window[s[left]] == 0:
+                del window[s[left]]  
 
-        # check the rest 
-        for i in range(len(p), len(s)):
-            cur_window[ord(s[i-len(p)])- a] -= 1
-            cur_window[ord(s[i]) - a] += 1
-            if cur_window == checker:
-                res.append(i - len(p) +1)
+            if window == cnt_p:
+                res.append(right - k + 1)
 
         return res
+
+
+           
