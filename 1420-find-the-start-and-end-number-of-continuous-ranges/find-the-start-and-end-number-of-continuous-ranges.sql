0360ad88c710bbd3d6a76476812ecff2f3dd02e6
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-26 16:03:27 -0600

    Time: 418 ms (44.14%) | Memory: 0B (100.00%) - LeetSync

--- 1420-find-the-start-and-end-number-of-continuous-ranges/find-the-start-and-end-number-of-continuous-ranges.sql
+++ 1420-find-the-start-and-end-number-of-continuous-ranges/find-the-start-and-end-number-of-continuous-ranges.sql
@@ -1,10 +1,10 @@
 WITH t AS(
     SELECT log_id,
-       ROW_NUMBER()OVER() as rn,
-       log_id - ROW_NUMBER()OVER() AS diff
+       ROW_NUMBER()OVER(ORDER BY log_id) AS rn,
+       log_id - (ROW_NUMBER()OVER(ORDER BY log_id)) AS diff
     FROM Logs
 )
 SELECT MIN(log_id) AS start_id,
        MAX(log_id) AS end_id
 FROM t
-GROUP BY diff
+GROUP BY diff;
