e67a327a7fa74060768a7a6f106d445f2e9b0d35
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-26 10:07:01 -0600

    Time: 39 ms (56.96%) | Memory: 37.6 MB (62.09%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -1,11 +1,13 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        d = {}
         res = []
-
-        copy_arr = (sorted(set(arr)))
-        for i, ele in enumerate(copy_arr):
-            d[ele] = i + 1
+        d = {}
+        cur = 1 
+        sort_arr = sorted(arr, reverse = False)
+        for ele in sort_arr:
+            if ele not in d:
+                d[ele] = cur
+                cur += 1
 
         for num in arr:
             res.append(d[num])
