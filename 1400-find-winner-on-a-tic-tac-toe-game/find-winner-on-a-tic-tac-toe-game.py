f2e5973b579b09cb1fe927c150f42e36bc9e3cdc
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-26 10:26:55 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (34.83%) - LeetSync

--- 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.py
+++ 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.py
@@ -1,48 +1,41 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        a_move, b_move = [], []
+        a_move = []
+        b_move = []
         for i, move in enumerate(moves):
             if i % 2 == 0:
                 a_move.append(move)
             else:
                 b_move.append(move)
 
-        # chk a, b wins
-        def check_win(moves):
-            moves = set(map(tuple, moves))
-
-            # rows
-            for r in range(3):
-                if all((r,c) in moves for c in range(3)):
+        def win_condition(moves):
+            moves = set(tuple(move) for move in moves)
+            # --
+            for row in range(3):
+                if all((row, col) in moves for col in range(3)):
+                    return True
+            # |
+            for col in range(3):
+                if all((row, col) in moves for row in range(3)):
                     return True
 
-            # 
-            for c in range(3):
-                if all((r,c) in moves for r in range(3)):
-                    return True
-
-            #
-            if all((i, i) in moves for i in range(3)):
+            #  \ 
+            if all((col, col) in moves for col in range(3)):
                 return True
 
-            # diagonal 右上到左下
-            if all((i, 2 - i) in moves for i in range(3)):
+            # /
+            if all((col, 2-col) in moves for col in range(3)):
                 return True
-
+            
             return False
 
-        # a = True--> a wins--> "A"
-        if check_win(a_move):
+
+        if win_condition(a_move):
             return "A"
-        # b = True --> b wins --> "B"
-        if check_win(b_move):
+        elif win_condition(b_move):
             return "B"
-        # both False chk length, if < 9 --> "Pending"
-        if not check_win(a_move) and not check_win(b_move) and (len(a_move) + len(b_move)) < 9:
-            return "Pending"
-        # both False chk length, if == 9 --> "Draw"
-        if not check_win(a_move) and not check_win(b_move) and (len(a_move) + len(b_move)) == 9:
+        elif len(moves) == 9:
             return "Draw"
-
-
-        
+        else:
+            return "Pending"
+            
