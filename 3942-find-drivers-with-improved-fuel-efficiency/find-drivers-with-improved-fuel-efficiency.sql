9c859283a36edeabd50e3e156b6be8d85cd4175d
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-26 18:41:55 -0600

    Time: 540 ms (59.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3942-find-drivers-with-improved-fuel-efficiency/find-drivers-with-improved-fuel-efficiency.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+WITH t AS(
+    SELECT driver_id,
+       AVG(CASE WHEN trip_date <= '2023-06-30' THEN (distance_km/fuel_consumed)END) AS first_half_avg,
+       AVG(CASE WHEN trip_date > '2023-06-30' THEN (distance_km/fuel_consumed)END) AS second_half_avg
+    FROM trips
+    GROUP BY driver_id
+)
+SELECT d.driver_id,
+       d.driver_name,
+       ROUND(t.first_half_avg, 2) AS first_half_avg,
+       ROUND(t.second_half_avg, 2) AS second_half_avg,
+       ROUND(t.second_half_avg - t.first_half_avg, 2) AS efficiency_improvement
+FROM t
+JOIN drivers d
+ON t.driver_id = d.driver_id
+WHERE t.first_half_avg IS NOT NULL AND t.second_half_avg IS NOT NULL AND ROUND(t.second_half_avg - t.first_half_avg, 2) > 0
+ORDER BY efficiency_improvement DESC, d.driver_name ASC;
+
+
