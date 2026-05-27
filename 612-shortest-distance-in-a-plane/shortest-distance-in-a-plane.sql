992cc2f06ade202d3f0cd245eb84370520ce6e1d
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-27 10:33:42 -0600

    Time: 252 ms (31.39%) | Memory: 0B (100.00%) - LeetSync

--- 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
+++ 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT ROUND(MIN(SQRT(POWER(p2.x - p1.x, 2) + POWER(p2.y - p1.y, 2))),2) AS shortest
+SELECT MIN(ROUND(SQRT(POWER((p2.x - p1.x),2) + POWER((p2.y - p1.y),2)),2))AS shortest
 FROM Point2D p1
 JOIN Point2D p2
-ON p1.x != p2.x OR p1.y != p2.y;
+WHERE NOT (p1.x = p2.x AND p1.y = p2.y) 
