53b74b07adf72293c85eca4ae07a6e33dc383e7d
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-28 12:16:44 -0600

    Time: 688 ms (8.61%) | Memory: 0B (100.00%) - LeetSync

--- 3932-find-covid-recovery-patients/find-covid-recovery-patients.sql
+++ 3932-find-covid-recovery-patients/find-covid-recovery-patients.sql
@@ -1,40 +1,32 @@
 # Write your MySQL query statement below
 WITH t AS(
-    SELECT patient_id
+    SELECT *,
+           MIN(test_date) AS min_pos_date
     FROM covid_tests
-    WHERE result IN ("Positive","Negative")
+    WHERE result = 'Positive'
     GROUP BY patient_id
-    HAVING COUNT(DISTINCT result) >= 2
-), 
-pos AS(
+), a AS(
     SELECT c.patient_id,
-           MIN(c.test_date) AS min_pos
-FROM covid_tests c
-WHERE result = 'Positive'
-GROUP BY c.patient_id
-
-),
-neg AS(
-    SELECT c.patient_id,
-           MIN(c.test_date) AS min_neg
-FROM covid_tests c
-JOIN pos
-ON c.patient_id = pos.patient_id
-WHERE result = 'Negative' AND test_date > pos.min_pos
-GROUP BY c.patient_id
-)
-
-
+       t.min_pos_date,
+       MIN(c.test_date) AS min_neg_date,
+       TIMESTAMPDIFF(day, t.min_pos_date,MIN(c.test_date)) AS diff
+    FROM covid_tests c
+    LEFT JOIN t
+    ON c.patient_id = t.patient_id
+    WHERE c.result = 'Negative' AND TIMESTAMPDIFF(day, t.min_pos_date,c.test_date) > 0
+    GROUP BY c.patient_id)
 SELECT p.patient_id,
        p.patient_name,
        p.age,
-       DATEDIFF(neg.min_neg, pos.min_pos) AS recovery_time
+       a.diff AS recovery_time
 FROM patients p
-JOIN t
-ON p.patient_id = t.patient_id
-JOIN pos
-ON p.patient_id = pos.patient_id
-JOIN neg
-ON p.patient_id = neg.patient_id
-
+JOIN a 
+ON p.patient_id = a.patient_id
 ORDER BY recovery_time ASC, p.patient_name ASC;
+
+
+
+
+
+    
+
