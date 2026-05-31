128da7dcfd72ee1d2ec27f253265ca7659291787
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-05-31 13:04:18 -0600

    Time: 287 ms (72.37%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,12 +1,3 @@
-# Write your MySQL query statement below
-# x+ y > z and y+z > x and x+z > y
-# 但我不知道要怎麼寫相加的邏輯 還有新增triangle 以及裡面賦值
-
-SELECT x,
-       y,
-       z,
-       IF (x + y > z AND x + z > y AND y + z > x,
-        "Yes",
-        "No"
-       )AS triangle
+SELECT *,
+       CASE WHEN (x+ y > z )AND(y+ z > x) AND (x+ z >y) THEN 'Yes'ELSE 'No' END AS triangle
 FROM Triangle
