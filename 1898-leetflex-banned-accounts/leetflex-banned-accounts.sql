669c9c2f0cc3c98c7ba7e0ff0554b4dc4a8c56ca
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-01 15:28:44 -0600

    Time: 587 ms (19.39%) | Memory: 0B (100.00%) - LeetSync

--- 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
+++ 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -2,5 +2,9 @@
 SELECT DISTINCT l1.account_id
 FROM LogInfo l1
 JOIN LogInfo l2
-ON l1.account_id = l2.account_id AND l1.ip_address != l2.ip_address
-WHERE l1.logout >= l2.login AND l1.login < l2.login
+ON l1.account_id = l2.account_id
+WHERE l1.ip_address != l2.ip_address
+      AND
+      l1.login > l2.login # l1比較晚開始
+      AND 
+      TIMESTAMPDIFF(SECOND, l1.login , l2.logout) >= 0
