c7bbfc75f85791f006fb7a98554bcea31da64db1
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-02 14:34:39 -0600

    Time: 523 ms (33.37%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,5 @@
-SELECT activity_date AS day,
+SELECT activity_date AS `day`,
        COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE DATEDIFF('2019-07-27', activity_date) BETWEEN 0 AND 29
-GROUP BY activity_date;
+WHERE TIMESTAMPDIFF(day, activity_date, '2019-07-27') BETWEEN 0 AND 29
+GROUP BY activity_date
