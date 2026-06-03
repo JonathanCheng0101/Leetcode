5aac3a1717a7fd419b6071d6e68385654652c35e
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-03 15:26:25 -0600

    Time: 368 ms (80.70%) | Memory: 0B (100.00%) - LeetSync

--- 1504-get-the-second-most-recent-activity/get-the-second-most-recent-activity.sql
+++ 1504-get-the-second-most-recent-activity/get-the-second-most-recent-activity.sql
@@ -1,19 +1,15 @@
 # Write your MySQL query statement below
 WITH t AS(
     SELECT *,
-           ROW_NUMBER()OVER(PARTITION BY username ORDER BY endDate DESC) AS rn
+           ROW_NUMBER()OVER(PARTITION BY username ORDER BY endDate DESC) AS rn,
+           COUNT(username) OVER(PARTITION BY username) AS cnt
     FROM UserActivity
 )
 
 SELECT username, activity, startDate, endDate
 FROM t
-WHERE rn = 2
+WHERE rn = 2 or cnt = 1
 
-UNION ALL
 
-SELECT username, activity, startDate, endDate
-FROM UserActivity
-GROUP BY username
-HAVING COUNT(*) = 1
 
 
