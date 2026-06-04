23ceff327633b33600dc3b71d3bfeb726e5cf5f2
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-04 16:29:57 -0600

    Time: 791 ms (64.42%) | Memory: 0B (100.00%) - LeetSync

--- 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
+++ 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
@@ -1,17 +1,12 @@
-# Write your MySQL query statement below
-WITH a AS(
-    SELECT t.team_id,
-        t.name,
-        CAST(RANK()OVER(ORDER BY t.points DESC, t.name ASC) AS SIGNED) AS cur_rank,
-        t.points + c.points_change AS nxt_point
-    FROM Teampoints t
-    JOIN PointsChange c
-    ON t.team_id = c.team_id
+WITH t AS(
+    SELECT t.team_id, t.name,t.points,p.points_change,
+        CAST(RANK()OVER(ORDER BY t.points DESC, t.name ASC) AS SIGNED) AS cur_rn,
+       t.points + p.points_change AS new_points
+    FROM TeamPoints t
+    JOIN PointsChange p
+    ON t.team_id = p.team_id
 )
-
-    SELECT team_id,
+SELECT team_id,
        name,
-       cur_rank - CAST(RANK()OVER(ORDER BY a.nxt_point DESC, a.name ASC)AS SIGNED) AS rank_diff
-FROM a
-
-
+       cur_rn - CAST(RANK()OVER(ORDER BY new_points DESC, t.name ASC) AS SIGNED) AS rank_diff
+FROM t
