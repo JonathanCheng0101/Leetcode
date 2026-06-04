a0266f99b421c9df149f3f151ef512c7ae7b8fef
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-04 14:16:44 -0600

    Time: 570 ms (83.33%) | Memory: 0B (100.00%) - LeetSync

--- 1425-weather-type-in-each-country/weather-type-in-each-country.sql
+++ 1425-weather-type-in-each-country/weather-type-in-each-country.sql
@@ -1,12 +1,13 @@
-# Write your MySQL query statement below
-# Countries Weather
-SELECT country_name,
-       (CASE WHEN AVG(weather_state) <=15 THEN 'Cold'
-            WHEN AVG(weather_state) >= 25 THEN 'Hot'
-            ELSE 'Warm' END)
-            AS weather_type
+WITH t AS(
+    SELECT country_id,
+        CASE WHEN AVG(weather_state) <= 15 THEN 'Cold' 
+             WHEN AVG(weather_state) >= 25 THEN 'Hot'
+            ELSE 'Warm' END AS weather_type
+    FROM Weather
+    WHERE DATE_FORMAT(`day`, "%Y-%m") = '2019-11'
+    GROUP BY country_id
+)
+SELECT  c.country_name, t.weather_type
 FROM Countries c
-JOIN Weather w
-ON c.country_id = w.country_id
-WHERE w.day BETWEEN '2019-11-01' AND '2019-11-30' 
-GROUP BY w.country_id;
+JOIN t 
+ON c.country_id = t.country_id
