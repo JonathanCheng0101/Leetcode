ccc0d52e25b5af4978c024c78626ddd86cf11def
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-04 19:17:31 -0600

    Time: 147 ms (23.93%) | Memory: 38.1 MB (5.20%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,27 +1,31 @@
 from collections import Counter
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        window = nums[:k]
+        cnt = Counter(window)
+        cur_sum = sum(window)
         res = 0
 
-        window = Counter(nums[:k])
-        cur_sum = sum(nums[:k])
-
-        if len(window) == k:
-            res = sum(window)
+        if len(cnt) == k:
+            res = cur_sum
 
         for i in range(k, len(nums)):
             incoming = nums[i]
             outgoing = nums[i - k]
 
-            window[outgoing] -= 1
-            if window[outgoing] == 0:
-                del window[outgoing]
-            window[incoming] = window.get(incoming, 0) + 1 
-
+            # delete old
+            cnt[outgoing] -= 1
+            if cnt[outgoing] == 0:
+                del cnt[outgoing]
             cur_sum -= outgoing
+
+            # add new
+            cnt[incoming] = cnt.get(incoming, 0) + 1
             cur_sum += incoming
 
-            if len(window) == k:
+            # chk distinct
+            if len(cnt) == k:
                 res = max(res, cur_sum)
 
         return res
+   
