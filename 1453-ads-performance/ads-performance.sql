344251e544c9ac8b44035a57855adc966275d318
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-05 13:37:52 -0600

    Time: 538 ms (83.07%) | Memory: 0B (100.00%) - LeetSync

--- 1453-ads-performance/ads-performance.sql
+++ 1453-ads-performance/ads-performance.sql
@@ -1,6 +1,11 @@
-# Write your MySQL query statement below
+WITH t AS(
+    SELECT ad_id,
+        SUM(CASE WHEN action = 'Clicked' THEN 1 ELSE 0 END) AS total_click,
+        SUM(CASE WHEN action = 'Viewed' THEN 1 ELSE 0 END) AS total_viewed
+    FROM Ads
+    GROUP BY ad_id
+)
 SELECT ad_id,
-       IFNULL(ROUND(SUM(CASE WHEN action = 'clicked' THEN 1 ELSE 0 END)*100/ SUM(CASE WHEN action IN ('clicked', 'viewed') THEN 1 ELSE 0 END) , 2),0) AS ctr
-FROM Ads
-GROUP BY ad_id
+       CASE WHEN total_click + total_viewed  = 0 THEN 0 ELSE ROUND(100 * total_click /(total_click + total_viewed), 2) END AS ctr
+FROM t
 ORDER BY ctr DESC, ad_id ASC;
