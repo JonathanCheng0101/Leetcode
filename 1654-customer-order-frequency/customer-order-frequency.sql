ac218404034771a980d31b46ab52deeb10239a2c
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-07 14:59:29 -0600

    Time: 1724 ms (5.30%) | Memory: 0B (100.00%) - LeetSync

--- 1654-customer-order-frequency/customer-order-frequency.sql
+++ 1654-customer-order-frequency/customer-order-frequency.sql
@@ -1,20 +1,27 @@
+# Write your MySQL query statement below
+WITH t AS(
+    SELECT o.customer_id,
+        DATE_FORMAT(o.order_date, "%Y-%m") AS `date`,
+        p.price * o.quantity AS amount
+    FROM Orders o
+    JOIN Product p
+    ON o.product_id = p.product_id
+), a AS(
+    SELECT customer_id,
+       `date`,
+       SUM(amount) AS amount
+FROM t
+WHERE `date` = '2020-06' OR `date` = '2020-07'
+GROUP BY customer_id, `date`
+
+), b AS(
+    SELECT *,
+        CASE WHEN (`date` = '2020-06' OR `date` = '2020-07') AND AMOUNT >=100 THEN 1 ELSE 0 END AS chk
+FROM a
+)
 SELECT c.customer_id, c.name
-FROM Customers c
-JOIN Orders o
-ON c.customer_id = o.customer_id
-JOIN Product p
-ON o.product_id = p.product_id
-GROUP BY o.customer_id
-HAVING SUM(
-       CASE WHEN o.order_date BETWEEN '2020-06-01' AND '2020-06-30' 
-       THEN o.quantity * p.price 
-       ELSE 0 END)
-       >=100 
-       AND
-       SUM(
-       CASE WHEN o.order_date BETWEEN '2020-07-01' AND '2020-07-31' 
-       THEN o.quantity * p.price 
-       ELSE 0 END) 
-       >= 100;
-
-
+FROM b
+JOIN Customers c
+ON b.customer_id = c.customer_id
+GROUP BY b.customer_id
+HAVING SUM(chk) = 2 
