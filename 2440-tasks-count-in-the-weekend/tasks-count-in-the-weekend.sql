61ba115eeed35af55be29df72b2742285a698ca7
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-07 17:18:09 -0600

    Time: 454 ms (42.86%) | Memory: 0B (100.00%) - LeetSync

--- 2440-tasks-count-in-the-weekend/tasks-count-in-the-weekend.sql
+++ 2440-tasks-count-in-the-weekend/tasks-count-in-the-weekend.sql
@@ -1,10 +1,8 @@
-# Write your MySQL query statement below
-# DAYOFWEEK(date)
 WITH t AS(
-    SELECT task_id,
-       DAYOFWEEK(submit_date) AS judge
-    FROM Tasks
+    SELECT submit_date,
+        DAYOFWEEK(submit_date) AS day_chk
+FROM Tasks
 )
-SELECT SUM(CASE WHEN judge = 1 OR judge = 7 THEN 1 ELSE 0 END) AS weekend_cnt,
-       SUM(CASE WHEN judge BETWEEN 2 AND 6 THEN 1 ELSE 0 END) AS working_cnt 
+SELECT SUM(CASE WHEN day_chk = 1 OR day_chk = 7 THEN 1 ELSE 0 END) AS weekend_cnt,
+       SUM(CASE WHEN day_chk BETWEEN 2 AND 6 THEN 1 ELSE 0 END) AS working_cnt
 FROM t
