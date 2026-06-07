1dd530b4318a65015cbff4590e57a80b33e1011f
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-07 17:15:16 -0600

    Time: 496 ms (78.57%) | Memory: 0B (100.00%) - LeetSync

--- 2431-products-with-three-or-more-orders-in-two-consecutive-years/products-with-three-or-more-orders-in-two-consecutive-years.sql
+++ 2431-products-with-three-or-more-orders-in-two-consecutive-years/products-with-three-or-more-orders-in-two-consecutive-years.sql
@@ -1,19 +1,16 @@
 # Write your MySQL query statement below
 WITH t AS(
-    SELECT product_id,
-        YEAR(purchase_date) AS `year`,
-        COUNT(product_id) AS cnt
-    FROM Orders
-    GROUP BY product_id, YEAR(purchase_date)
+    SELECT product_id, YEAR(purchase_date) AS `year`, COUNT(product_id) AS cnt
+FROM Orders
+GROUP BY product_id, `year`
 ), a AS(
-    SELECT *,
-       LEAD(cnt)OVER(PARTITION BY product_id ORDER BY `year`) AS nxt_cnt,
-       LEAD(`year`)OVER(PARTITION BY product_id ORDER BY `year`) AS nxt_yr     
-    FROM t
+    SELECT product_id,
+       `year`,
+       cnt,
+       LEAD(`year`)OVER(PARTITION BY product_id ORDER BY `year` ASC) AS nxt_yr,
+       LEAD(cnt)OVER(PARTITION BY product_id ORDER BY `year` ASC) AS nxt_cnt
+FROM t
 )
-
 SELECT DISTINCT product_id
 FROM a
-WHERE cnt >= 3 AND
-      nxt_cnt >= 3 AND
-      nxt_yr - `year` = 1;
+WHERE nxt_yr - `year` = 1 AND cnt >= 3 AND nxt_cnt >= 3;
