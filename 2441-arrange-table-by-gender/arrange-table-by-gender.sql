4cdb3d7f425616ed81a17505edd98e0fd307506e
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-07 17:28:48 -0600

    Time: 504 ms (33.33%) | Memory: 0B (100.00%) - LeetSync

--- 2441-arrange-table-by-gender/arrange-table-by-gender.sql
+++ 2441-arrange-table-by-gender/arrange-table-by-gender.sql
@@ -1,12 +1,13 @@
 # Write your MySQL query statement below
 WITH t AS(
-    SELECT user_id,
-       gender,
-       ROW_NUMBER() OVER(PARTITION BY gender ORDER BY user_id) AS rn
+    SELECT *,
+            CASE WHEN gender = 'female' THEN 1
+                WHEN gender = 'other' THEN 2
+                WHEN gender = 'male' THEN 3 END AS gender_chk,
+            ROW_NUMBER()OVER(PARTITION BY gender ORDER BY user_id ASC) AS rn
     FROM Genders
 )
 SELECT user_id,
        gender
 FROM t
-ORDER BY rn ASC, gender 
-
+ORDER BY rn ASC, gender_chk ASC;
