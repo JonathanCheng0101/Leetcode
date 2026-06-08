ad6cc24c9669d39fc9e79f875445ace535c8c378
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-08 13:55:44 -0600

    Time: 2883 ms (16.16%) | Memory: 0B (100.00%) - LeetSync

--- 1763-all-valid-triplets-that-can-represent-a-country/all-valid-triplets-that-can-represent-a-country.sql
+++ 1763-all-valid-triplets-that-can-represent-a-country/all-valid-triplets-that-can-represent-a-country.sql
@@ -1,10 +1,16 @@
-# Write your MySQL query statement below
-SELECT a.student_name AS member_A,
-       b.student_name AS member_B,
-       c.student_name AS member_C
+WITH t AS(
+    SELECT a.student_id AS a_id,
+       a.student_name AS a_name,
+       b.student_id AS b_id,
+       b.student_name AS b_name,
+       c.student_id AS c_id,
+       c.student_name AS c_name
 FROM SchoolA a
 JOIN SchoolB b
 JOIN SchoolC c
-WHERE  (a.student_id != b.student_id) AND (b.student_id != c.student_id) AND (a.student_id != c.student_id) 
-       AND
-       (a.student_name != b.student_name) AND (b.student_name != c.student_name) AND (a.student_name != c.student_name);
+)
+SELECT a_name AS member_A,
+       b_name AS member_B,
+       C_name AS member_C
+FROM t
+WHERE (a_id != b_id) AND(b_id != c_id) AND(a_id != c_id) AND (a_name != b_name)AND (b_name != c_name)AND (a_name != c_name)
