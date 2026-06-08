723956d823b417c2d2e2c71412015a0292978e53
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-08 13:23:57 -0600

    Time: 895 ms (89.75%) | Memory: 0B (100.00%) - LeetSync

--- 1749-sellers-with-no-sales/sellers-with-no-sales.sql
+++ 1749-sellers-with-no-sales/sellers-with-no-sales.sql
@@ -1,11 +1,8 @@
-# Write your MySQL query statement below
 SELECT s.seller_name
 FROM Seller s
-LEFT JOIN Orders o
-ON s.seller_id = o.seller_id
-WHERE o.seller_id NOT IN(
-    SELECT DISTINCT seller_id
-    FROM Orders
-    WHERE DATE_FORMAT(sale_date,'%Y') = '2020'
-) OR o.seller_id IS NULL
+WHERE NOT EXISTS(
+    SELECT 1
+    FROM Orders o
+    WHERE s.seller_id = o.seller_id AND YEAR(o.sale_date) = 2020
+)
 ORDER BY s.seller_name ASC;
