8705920322c7149855dd2cc5f43f7ed834ec67e5
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-09 13:53:06 -0600

    Time: 305 ms (43.48%) | Memory: 0B (100.00%) - LeetSync

--- 3156-rolling-average-steps/rolling-average-steps.sql
+++ 3156-rolling-average-steps/rolling-average-steps.sql
@@ -1,27 +1,24 @@
 # Write your MySQL query statement below
 WITH t AS(
     SELECT *,
-       DAY(steps_date) - ROW_NUMBER()OVER(PARTITION BY user_id ORDER BY DAY(steps_date)ASC) AS rn
+        DATE_SUB(steps_date, INTERVAL ROW_NUMBER()OVER(PARTITION BY user_id ORDER BY steps_date) day) AS grp
     FROM Steps
 ), a AS(
-    SELECT *, COUNT(rn)OVER(PARTITION BY user_id, rn) AS cnt
-    FROM t
+SELECT *, COUNT(grp)OVER(PARTITION BY user_id,grp) AS grp_cnt
+FROM t 
 ), b AS(
     SELECT user_id,
        steps_count,
        steps_date,
-       rn,
-       DATE_ADD(MIN(steps_date)OVER(PARTITION BY user_id, rn), INTERVAL 2 day) AS min_date,
-       ROUND(SUM(steps_count)OVER(PARTITION BY user_id, rn ORDER BY steps_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)/ 3, 2) AS rolling_average
-    FROM a
-    WHERE cnt >= 3
-
+       ROUND(SUM(steps_count)OVER(PARTITION BY user_id, grp ORDER BY steps_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) / 3, 2) AS rolling_average,
+       DATE_ADD(MIN(steps_date)OVER(PARTITION BY user_id, grp), INTERVAL 2 day) AS valid_day
+FROM a
+WHERE grp_cnt >= 3
 )
-    
+
 SELECT user_id,
        steps_date,
        rolling_average
 FROM b
-WHERE steps_date >= min_date
+WHERE steps_date >= valid_day
 ORDER BY user_id ASC, steps_date ASC;
-
