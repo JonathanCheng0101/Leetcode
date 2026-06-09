8c25a79d5e4fc112d8a6a657a6544f7b6a77e3fc
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-09 13:27:10 -0600

    Time: 373 ms (11.76%) | Memory: 0B (100.00%) - LeetSync

--- 2885-count-occurrences-in-text/count-occurrences-in-text.sql
+++ 2885-count-occurrences-in-text/count-occurrences-in-text.sql
@@ -1,15 +1,13 @@
-# Write your MySQL query statement below
 WITH t AS(
     SELECT SUM(CASE WHEN content REGEXP '( )bull( )' THEN 1 ELSE 0 END) AS bull_cnt,
-           SUM(CASE WHEN content REGEXP '( )bear( )' THEN 1 ELSE 0 END) AS bear_cnt
+        SUM(CASE WHEN content REGEXP '( )bear( )' THEN 1 ELSE 0 END) AS bear_cnt
     FROM Files
 )
-
 SELECT 'bull' AS word,
-        bull_cnt AS `count`
+       bull_cnt AS `count`
 FROM t
 
-UNION ALL
+UNION ALL 
 
 SELECT 'bear',
        bear_cnt
