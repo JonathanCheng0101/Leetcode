b7b8ce465d820f21da47de3dd57b24a4ebdec80b
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-10 14:39:15 -0600

    Time: 1108 ms (15.19%) | Memory: 0B (100.00%) - LeetSync

--- 3002-flight-occupancy-and-waitlist-analysis/flight-occupancy-and-waitlist-analysis.sql
+++ 3002-flight-occupancy-and-waitlist-analysis/flight-occupancy-and-waitlist-analysis.sql
@@ -1,14 +1,19 @@
 # Write your MySQL query statement below
 WITH t AS(
-    SELECT flight_id, COUNT(flight_id) AS cnt
+    SELECT flight_id,
+        COUNT(*) AS cnt
     FROM Passengers
     GROUP BY flight_id
+), a AS(
+    SELECT f.flight_id,
+        f.capacity,
+        IFNULL(t.cnt, 0) AS cnt
+    FROM Flights f
+    LEFT JOIN t
+    ON f.flight_id = t.flight_id
 )
-SELECT f.flight_id,
-       IFNULL(LEAST(f.capacity, t.cnt), 0) AS booked_cnt,
-       IFNULL(CASE WHEN t.cnt - f.capacity > 0 THEN t.cnt - f.capacity
-                   WHEN t.cnt - f.capacity < 0 THEN 0 END, 0)AS waitlist_cnt
-FROM Flights f
-LEFT JOIN t
-ON f.flight_id = t.flight_id
-ORDER BY f.flight_id ASC;
+SELECT flight_id,
+       LEAST(capacity, cnt) AS booked_cnt,
+       GREATEST(0, cnt - capacity) AS waitlist_cnt
+FROM a
+ORDER BY flight_id ASC;
