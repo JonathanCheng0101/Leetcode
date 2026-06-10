ecb3644dbf51e205b7a4b02f97cf6bfd24b30d2c
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-10 14:11:11 -0600

    Time: 281 ms (90.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3253-symmetric-coordinates/symmetric-coordinates.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT LEAST(c1.`X` , c1.`Y`) AS 'x',
+       GREATEST(c1.`X` , c1.`Y`) AS 'y'
+FROM Coordinates c1
+JOIN Coordinates c2
+ON c1.`X` = c2.`Y` AND c2.`X` = c1.`Y`
+GROUP BY LEAST(c1.`X` , c1.`Y`), GREATEST(c1.`X` , c1.`Y`)
+HAVING COUNT(*)> 1
+ORDER BY LEAST(c1.`X` , c1.`Y`) ASC,
+         GREATEST(c1.`X` , c1.`Y`) ASC;
