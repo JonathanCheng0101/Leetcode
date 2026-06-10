0b7b4c7fc1c2c981d0c8c6b64df5ae3e363a5bf6
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-09 18:38:09 -0600

    Time: 3 ms (38.69%) | Memory: 19.3 MB (27.37%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.py
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -2,13 +2,15 @@ from heapq import heappush, heappop, heapify
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
         res = []
-
         heapify(nums)
-        while nums:
-            alice = heappop(nums)
-            bob = heappop(nums)
+        while len(nums)> 0:
+            a = heappop(nums)
+            
+            if len(nums)> 0:
+                b = heappop(nums)
+            res.append(b)   
+            res.append(a)
+            
 
-            res.append(bob)
-            res.append(alice)
 
         return res
