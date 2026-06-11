0e7c2157702730530ce3f0fd7f7a8c4e9e11ae60
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-11 14:27:37 -0600

    Time: 326 ms (46.99%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3255-pizza-toppings-cost-analysis/pizza-toppings-cost-analysis.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(t1.topping_name,',', t2.topping_name,',', t3.topping_name) AS pizza,
+       t1.cost + t2.cost + t3.cost AS total_cost
+FROM Toppings t1
+JOIN Toppings t2
+ON t1.topping_name < t2.topping_name
+JOIN Toppings t3
+ON t1.topping_name < t3.topping_name AND t2.topping_name < t3.topping_name
+ORDER BY total_cost DESC, pizza ASC;
