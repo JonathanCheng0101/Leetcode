a8f7273eeb94fdd36834686243660ea4f32c210e
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-11 15:44:35 -0600

    Time: 525 ms (86.49%) | Memory: 0B (100.00%) - LeetSync

--- 3283-find-third-transaction/find-third-transaction.sql
+++ 3283-find-third-transaction/find-third-transaction.sql
@@ -1,25 +1,19 @@
 # Write your MySQL query statement below
 WITH t AS(
     SELECT *,
-        COUNT(user_id) OVER(PARTITION BY user_id) AS cnt,
-        ROW_NUMBER()OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS rn        
+        COUNT(user_id)OVER(PARTITION BY user_id) AS cnt
     FROM Transactions
 ), a AS(
-    SELECT *
-    FROM t
-    WHERE cnt >= 3 AND rn <=3
-), b AS(SELECT *,
-           LAG(spend)OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS prev_spend
-    FROM a
-)
-, c AS(
     SELECT *,
-       LAG(prev_spend)OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS prev2_spend
-FROM b
-) 
+        LAG(spend)OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS prev_spend,
+        LAG(spend, 2)OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS prev2_spend,
+        ROW_NUMBER()OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS rn
+    FROM t
+    WHERE cnt >=3
+)
 SELECT user_id,
        spend AS third_transaction_spend,
        transaction_date AS third_transaction_date
-FROM c
-WHERE rn = 3 AND spend > prev_spend AND spend > prev2_spend
+FROM a
+WHERE (rn = 3) AND (spend > prev_spend) AND (spend > prev2_spend) 
 ORDER BY user_id ASC;
