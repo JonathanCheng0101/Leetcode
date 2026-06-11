33f46fb6f1cd2a08b04a3c027d2293cedf8ae7f3
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-11 17:13:53 -0600

    Time: 15 ms (94.37%) | Memory: 19.4 MB (38.53%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
@@ -3,28 +3,23 @@ from heapq import heappop, heapify
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
         res = []
-        for i in range(k, len(nums)+1):
-            new_num = nums[i - k:i]
-            counter = Counter(new_num)
-
-            if len(counter) < x:
-                res.append(sum(new_num))
-                continue
-
-            cnt = 0
-
-            lst = [(-freq, -num) for num, freq in counter.items()]
-            heapify(lst)
-            
-            for _ in range(x):
-                freq, num = heappop(lst)
-                cnt += (-freq) *(-num)
-
-            res.append(cnt)
-
+        temp = 0
+        window = Counter(nums[:k])
+        sorted_window = sorted(window.items(), key = lambda x: (-x[1], -x[0]))[:x]
+        for [a,b] in sorted_window:
+            temp += a*b
+        res.append(temp)
+        temp = 0
+        l = 0
+        for r in range(k, len(nums)):
+            window[nums[r]] += 1
+            window[nums[l]] -= 1
+            l += 1
+            sorted_window = sorted(window.items(), key = lambda x: (-x[1], -x[0]))[:x]
+            for [a,b] in sorted_window:
+                temp += a*b
+            res.append(temp)
+            temp = 0
         return res
-
-        
-
-
+            
         
