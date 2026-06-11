ffe1cfbdbd6f5c10cceeaf91f945d1fe0dd480e6
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-11 13:22:00 -0600

    Time: 776 ms (20.73%) | Memory: 0B (100.00%) - LeetSync

--- 2092-users-that-actively-request-confirmation-messages/users-that-actively-request-confirmation-messages.sql
+++ 2092-users-that-actively-request-confirmation-messages/users-that-actively-request-confirmation-messages.sql
@@ -1,6 +1,8 @@
-# Write your MySQL query statement below
-# timestampdiff(hour, a,b)
-SELECT DISTINCT c1.user_id
-FROM Confirmations c1
-JOIN Confirmations c2
-ON c1.user_id = c2.user_id AND c1.time_stamp < c2.time_stamp AND TIMESTAMPDIFF(SECOND, c1.time_stamp, c2.time_stamp) <= 86400;
+WITH t AS(
+    SELECT user_id,
+        CASE WHEN TIMESTAMPDIFF(second, `time_stamp`, LEAD(`time_stamp`)OVER(PARTITION BY user_id ORDER BY `time_stamp` ASC)) <= 86400 THEN 1 ELSE 0 END AS chk
+    FROM Confirmations
+)
+SELECT DISTINCT user_id
+FROM t 
+WHERE chk = 1
