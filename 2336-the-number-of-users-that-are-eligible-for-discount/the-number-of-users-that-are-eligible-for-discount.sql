a4cd6d297b7fd5517a89bb7afca5fc74fd68c1ca
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-13 11:40:40 -0600

    Time: 673 ms (21.52%) | Memory: 0B (100.00%) - LeetSync

--- 2336-the-number-of-users-that-are-eligible-for-discount/the-number-of-users-that-are-eligible-for-discount.sql
+++ 2336-the-number-of-users-that-are-eligible-for-discount/the-number-of-users-that-are-eligible-for-discount.sql
@@ -2,8 +2,10 @@ CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS 
 BEGIN
   RETURN (
       # Write your MySQL query statement below.
-      SELECT COUNT(DISTINCT user_id) AS user_cnt
+      SELECT COUNT(DISTINCT user_id)
       FROM Purchases
-      WHERE amount >= minAmount AND (time_stamp BETWEEN DATE_FORMAT(startDate, '%Y-%m-%d') AND DATE_FORMAT(endDate, '%Y-%m-%d'))
+      WHERE time_stamp BETWEEN DATE_FORMAT(startDate, "%Y-%m-%d") AND DATE_FORMAT(endDate, "%Y-%m-%d")
+    AND amount >= minAmount
+      
   );
 END
