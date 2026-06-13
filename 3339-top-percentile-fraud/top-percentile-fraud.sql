3e96e854660eae8054739871a0adbb6f5b6f077f
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-13 14:24:19 -0600

    Time: 328 ms (54.05%) | Memory: 0B (100.00%) - LeetSync

--- 3339-top-percentile-fraud/top-percentile-fraud.sql
+++ 3339-top-percentile-fraud/top-percentile-fraud.sql
@@ -2,10 +2,10 @@
 WITH t AS(
     SELECT *,
         RANK()OVER(PARTITION BY state ORDER BY fraud_score DESC) AS rn,
-        COUNT(fraud_score)OVER(PARTITION BY state) AS cnt
+        CEIL(COUNT(state)OVER(PARTITION BY state) * 0.05) AS cnt
     FROM Fraud
 )
 SELECT policy_id, state, fraud_score
 FROM t
-WHERE rn <= CEIL(cnt * 0.05)
+WHERE rn <= cnt
 ORDER BY state ASC, fraud_score DESC, policy_id ASC;
