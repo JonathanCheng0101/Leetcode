79683ac78df7b91f1410480b9ef002ab7306ec87
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-13 14:30:10 -0600

    Time: 603 ms (26.14%) | Memory: 0B (100.00%) - LeetSync

--- 3340-snaps-analysis/snaps-analysis.sql
+++ 3340-snaps-analysis/snaps-analysis.sql
@@ -1,20 +1,12 @@
 # Write your MySQL query statement below
 WITH t AS(
-    SELECT user_id, activity_type, SUM(time_spent) AS time_spent
-    FROM Activities
-    GROUP BY user_id, activity_type
-), b AS(
-    SELECT user_id,
-       SUM(CASE WHEN activity_type = 'send' THEN IFNULL(time_spent, 0) END) AS send_perc,
-       SUM(CASE WHEN activity_type = 'open' THEN IFNULL(time_spent, 0) END) AS open_perc
-    FROM t
-    GROUP BY user_id
+    SELECT Age.age_bucket, a.user_id, a.activity_type, a.time_spent
+    FROM Activities a
+    JOIN Age 
+    ON a.user_id = Age.user_id
 )
-    
 SELECT age_bucket,
-        ROUND(100 * IFNULL(SUM(send_perc), 0)/(IFNULL(SUM(send_perc), 0) + IFNULL(SUM(open_perc), 0)), 2) AS send_perc,
-        ROUND(100 * IFNULL(SUM(open_perc), 0)/(IFNULL(SUM(send_perc), 0) + IFNULL(SUM(open_perc), 0)), 2) AS open_perc
-FROM Age a
-JOIN b
-ON a.user_id = b.user_id
-GROUP BY a.age_bucket
+       ROUND(100 * SUM(CASE WHEN activity_type = 'send' THEN time_spent ELSE 0 END)/ SUM(time_spent), 2) AS send_perc,
+       ROUND(100 * SUM(CASE WHEN activity_type = 'open' THEN time_spent ELSE 0 END)/ SUM(time_spent), 2) AS open_perc
+FROM t
+GROUP BY age_bucket
