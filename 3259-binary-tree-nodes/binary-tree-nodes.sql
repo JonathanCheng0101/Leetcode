5907aa3ee224306fa7adc3c91f0ab3e0578675b3
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-13 14:13:58 -0600

    Time: 289 ms (65.82%) | Memory: 0B (100.00%) - LeetSync

--- 3259-binary-tree-nodes/binary-tree-nodes.sql
+++ 3259-binary-tree-nodes/binary-tree-nodes.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 SELECT `N`,
-       CASE WHEN `P` IS NULL THEN 'Root'
-            WHEN `N` IN (SELECT `N` FROM Tree) AND `N` IN (SELECT `P` FROM Tree) THEN 'Inner'
-            ELSE 'Leaf' 
-       END AS Type
-FROM Tree
+        CASE WHEN `P` IS NULL THEN 'Root'
+             WHEN `N` IN (SELECT `N` FROM TREE) AND `N` IN (SELECT `P` FROM TREE) THEN 'Inner'
+             WHEN `N` IN (SELECT `N` FROM TREE) AND NOT EXISTS(SELECT 1 FROM TREE t2 WHERE t1.`N` = t2.`P`) THEN 'Leaf' END  AS Type
+
+FROM Tree t1
 ORDER BY `N` ASC;
