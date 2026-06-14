bed59786b3b645a9b51601c7a053cbceba97c248
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-13 19:05:49 -0600

    Time: 465 ms (29.69%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,8 @@
-SELECT MAX(num) AS num
-FROM (
-    SELECT num
+WITH t AS(
+    SELECT num AS num
     FROM MyNumbers
     GROUP BY num
-    HAVING COUNT(*) = 1
-    ) t;
+    HAVING COUNT(num) = 1
+)
+SELECT max(num) AS num
+FROM t
