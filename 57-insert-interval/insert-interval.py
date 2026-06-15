e4beed7a4c339a5af2cd5573f3fda1797c63bb4c
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-15 16:51:39 -0600

    Time: 0 ms (100.00%) | Memory: 21.5 MB (26.94%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,18 +1,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals.append(newInterval)
-        intervals = sorted(intervals, key = lambda x:x[0])
-                
-            
-
         res = []
-        for ele in intervals:
-            if len(res) == 0:
+
+        intervals.append(newInterval)
+        lst = sorted(intervals, key = lambda x: (x[0], x[1]))
+
+        for ele in lst:
+            if not res:
                 res.append(ele)
             else:
-                if res[-1][1] >= ele[0]:
-                    res[-1][1] = max(res[-1][1], ele[1])
-                else:
+                # non overlapping
+                # cur = res[-1]    nxt = ele 
+                if res[-1][1] < ele[0]:
                     res.append(ele)
+                else:
+                    res[-1][1] = max(res[-1][1], ele[1])
 
         return res
+
+        
