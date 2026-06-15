00c767450365b3841cb0cb6ff5755b87da9efe9e
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-15 12:57:05 -0600

    Time: 1703 ms (21.59%) | Memory: 0B (100.00%) - LeetSync

--- 2622-form-a-chemical-bond/form-a-chemical-bond.sql
+++ 2622-form-a-chemical-bond/form-a-chemical-bond.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
 SELECT e1.symbol AS metal,
-       e2.symbol AS nonmetal
+       e2.symbol AS Nonmetal
 FROM Elements e1
-JOIN Elements e2
-WHERE e1.type = 'Metal' AND e2.type = 'Nonmetal';
+CROSS JOIN Elements e2
+WHERE (e1.type = 'Metal' AND e2.type = 'Nonmetal')
