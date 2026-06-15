2adbd5fb8dbffc1c048a011243ce4080a933f470
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-15 16:23:50 -0600

    Time: 494 ms (45.59%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3358-friends-with-no-mutual-friends/friends-with-no-mutual-friends.sql
@@ -0,0 +1,21 @@
+WITH t AS(
+    SELECT user_id1 AS id1, user_id2 AS id2
+    FROM Friends
+    UNION ALL
+    SELECT user_id2, user_id1
+    FROM Friends
+), a AS(
+    SELECT DISTINCT
+           LEAST(t1.id2, t2.id2) AS user_id1,
+           GREATEST(t1.id2, t2.id2) AS user_id2
+    FROM t t1
+    JOIN t t2
+      ON t1.id1 = t2.id1
+     AND t1.id2 < t2.id2
+)
+SELECT f.user_id1, f.user_id2
+FROM Friends f
+LEFT JOIN a
+  ON LEAST(f.user_id1, f.user_id2) = a.user_id1 AND GREATEST(f.user_id1, f.user_id2) = a.user_id2
+WHERE a.user_id1 IS NULL
+ORDER BY f.user_id1, f.user_id2;
