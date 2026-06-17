82fd14243862619e8cf786478039af8f6f49b144
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-17 13:20:25 -0600

    Time: 369 ms (18.48%) | Memory: 0B (100.00%) - LeetSync

--- 3155-highest-salaries-difference/highest-salaries-difference.sql
+++ 3155-highest-salaries-difference/highest-salaries-difference.sql
@@ -1,12 +1,13 @@
-SELECT ABS(
-        MAX(
-        CASE WHEN department = 'Engineering'
-         THEN salary 
-         END) 
-         -
-         MAX(
-         CASE WHEN department = 'Marketing'
-          THEN salary 
-          END)
-          )AS salary_difference 
-FROM Salaries
+WITH t AS(
+    SELECT *,
+        RANK()OVER(PARTITION BY department ORDER BY salary DESC) AS rn
+    FROM Salaries
+), a AS(
+    SELECT department, salary
+    FROM t
+    WHERE rn = 1
+)
+SELECT ABS(a1.salary- a2.salary) AS salary_difference
+FROM a a1
+JOIN a a2
+ON a1.department = 'Marketing' AND a2.department = 'Engineering'
