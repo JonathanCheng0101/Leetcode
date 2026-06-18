58ac21e433f8aa6658b0f09018a30f90eca8d9fe
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-18 13:01:59 -0600

    Time: 281 ms (80.33%) | Memory: 0B (100.00%) - LeetSync

--- 3258-classifying-triangles-by-lengths/classifying-triangles-by-lengths.sql
+++ 3258-classifying-triangles-by-lengths/classifying-triangles-by-lengths.sql
@@ -1,8 +1,5 @@
-# Write your MySQL query statement below
-SELECT (CASE WHEN (A >= B + C) OR (B >= A + C) OR (C >= A + B) THEN 'Not A Triangle'
-             WHEN A = B AND B = C THEN 'Equilateral'
-             WHEN (A = B AND A != C) OR (B = C AND A != C) OR (A = C AND A != B) THEN 'Isosceles'
-             ELSE 'Scalene'
-             END
-             ) AS triangle_type
+SELECT CASE WHEN A = B  AND B = C AND C= A AND A != 0 THEN 'Equilateral'
+            WHEN ((A = B AND A != C) OR (B = C AND A != C)  OR(A = C AND B != C)) AND (A + B > C) AND (A + C > B) AND (C + B > A) THEN 'Isosceles'
+            WHEN (A != B) AND (B != C) AND (A != C) AND (A + B > C) AND (A + C > B) AND (C + B > A) THEN 'Scalene'
+            ELSE 'Not A Triangle' END AS triangle_type
 FROM Triangles;
