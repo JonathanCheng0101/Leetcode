ff4c4acdb6e0b64251afbfb9ee19a4949ff3765c
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-18 13:09:54 -0600

    Time: 299 ms (78.07%) | Memory: 0B (100.00%) - LeetSync

--- 3287-loan-types/loan-types.sql
+++ 3287-loan-types/loan-types.sql
@@ -1,7 +1,6 @@
-# Write your MySQL query statement below
 SELECT user_id
 FROM Loans
-WHERE loan_type IN ('Mortgage','Refinance')
+WHERE loan_type IN ('Mortgage', 'Refinance')
 GROUP BY user_id
 HAVING COUNT(DISTINCT loan_type) >= 2
 ORDER BY user_id ASC;
