ca377797d13eac9343431f08a2d17adbf1c02034
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-19 13:09:48 -0600

    Time: 535 ms (63.38%) | Memory: 0B (100.00%) - LeetSync

--- 3481-second-day-verification/second-day-verification.sql
+++ 3481-second-day-verification/second-day-verification.sql
@@ -1,7 +1,18 @@
-# Write your MySQL query statement below
-SELECT e.user_id
-FROM emails e
-JOIN texts t
-ON e.email_id = t.email_id
-WHERE t.signup_action = 'Verified' AND DATEDIFF(t.action_date, e.signup_date) = 1
-ORDER BY e.user_id ASC;
+WITH t AS(
+    SELECT e.email_id,
+        e.user_id,
+        DATE_FORMAT(e.signup_date, "%Y-%m-%d") AS signup_date,
+        t.signup_action,
+        DATE_FORMAT(t.action_date, "%Y-%m-%d") AS action_date
+    FROM emails e
+    JOIN texts t
+    ON e.email_id = t.email_id
+)
+SELECT user_id
+FROM t
+WHERE signup_action = 'Verified'
+      AND
+      TIMESTAMPDIFF(day, signup_date, action_date) = 1
+ORDER BY user_id ASC; 
+
+
