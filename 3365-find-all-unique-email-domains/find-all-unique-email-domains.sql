75958bce25be3796236508da30b0c6d62034b363
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-19 13:17:31 -0600

    Time: 305 ms (52.70%) | Memory: 0B (100.00%) - LeetSync

--- 3365-find-all-unique-email-domains/find-all-unique-email-domains.sql
+++ 3365-find-all-unique-email-domains/find-all-unique-email-domains.sql
@@ -1,6 +1,10 @@
-SELECT SUBSTRING_INDEX(email,'@',-1) AS email_domain,
-       COUNT(*) AS `count`
-FROM Emails
-WHERE email REGEXP '\\.com$'
-GROUP BY SUBSTRING_INDEX(email,'@',-1)
+WITH t AS(
+    SELECT *, SUBSTRING_INDEX(email, '@', -1) AS email_domain
+    FROM Emails
+    WHERE email LIKE '%.com'
+)
+SELECT email_domain,
+       COUNT(DISTINCT id) AS `count`
+FROM t
+GROUP BY email_domain
 ORDER BY email_domain ASC;
