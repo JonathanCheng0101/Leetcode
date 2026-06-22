49f8c1173da37bc171aee9c47e8612aa081c8cd4
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-22 12:49:34 -0600

    Time: 625 ms (34.00%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,9 +1,20 @@
 WITH t AS(
-    SELECT tiv_2016,
-           COUNT(*) OVER(PARTITION BY tiv_2015) AS cnt_2015,
-           COUNT(*) OVER(PARTITION BY lat,lon) AS latlon_cnt
+    SELECT pid, lat, lon
     FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(pid) = 1
+), a AS(
+    SELECT pid,
+           COUNT(tiv_2015)OVER(PARTITION BY tiv_2015) AS cnt
+    FROM Insurance
+    
 )
-SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016
-FROM t
-WHERE cnt_2015 > 1 AND latlon_cnt = 1
+SELECT ROUND(SUM(i.tiv_2016), 2) AS tiv_2016
+FROM Insurance i
+JOIN t
+ON i.pid = t.pid
+JOIN a
+ON i.pid = a.pid AND  a.cnt > 1
+
+
+
