952204acf772b1ddfc2829f0d93ec5c51ba9218d
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-23 16:19:51 -0600

    Time: 385 ms (83.37%) | Memory: 0B (100.00%) - LeetSync

--- 603-consecutive-available-seats/consecutive-available-seats.sql
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -1,12 +1,9 @@
-WITH t AS(
-    SELECT *
-    FROM Cinema
-    WHERE free = 1
-), a AS(
-SELECT CASE WHEN (seat_id - LAG(seat_id)OVER(ORDER BY seat_id) = 1) OR (LEAD(seat_id)OVER(ORDER BY seat_id) - seat_id= 1) THEN seat_id END AS seat_id
-FROM t
-)
-SELECT seat_id
-FROM a
-WHERE seat_id IS NOT NULL;
+SELECT c1.seat_id
+FROM Cinema c1
+LEFT JOIN Cinema c2
+ON c1.seat_id = c2.seat_id - 1
+LEFT JOIN Cinema c3
+ON c1.seat_id = c3.seat_id + 1
+WHERE c1.free = 1 AND (c2.free = 1 OR c3.free = 1)
+ORDER BY c1.seat_id
 
