d9f0b246e8132c3e918da4972b63a8929f8134bc
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-23 08:24:30 -0600

    Time: 1271 ms (18.01%) | Memory: 0B (100.00%) - LeetSync

--- 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
+++ 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
@@ -1,21 +1,13 @@
-# Write your MySQL query statement below
 WITH t AS(
-    SELECT e.left_operand,
-       v1.value AS `left`,
-       e.operator,
-       e.right_operand,
-       v2.value AS `right`
+    SELECT e.left_operand, v1.value AS `x`, e.operator, e.right_operand, v2.value AS `y`
     FROM Expressions e
     JOIN Variables v1
-    ON e.left_operand = v1.name
+    ON e.left_operand = v1.name 
     JOIN Variables v2
     ON e.right_operand = v2.name
 )
 SELECT left_operand,
        operator,
        right_operand,
-       CASE WHEN operator = '>' AND `left` > `right` THEN 'true'
-            WHEN operator = '=' AND `left` = `right` THEN 'true'
-            WHEN operator = '<' AND `left` < `right` THEN 'true'
-       ELSE 'false' END AS value
+       CASE WHEN (operator = '>' AND `x`>`y`) OR (operator = '=' AND `x`=`y`) OR (operator = '<' AND `x`<`y`) THEN 'true' ELSE 'false' END AS value
 FROM t
