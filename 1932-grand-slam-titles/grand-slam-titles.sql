c8b877aa9a92b9ec4f1a306435293e170dc618ee
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-26 13:50:38 -0600

    Time: 1444 ms (80.34%) | Memory: 0B (100.00%) - LeetSync

--- 1932-grand-slam-titles/grand-slam-titles.sql
+++ 1932-grand-slam-titles/grand-slam-titles.sql
@@ -1,27 +1,21 @@
-# Write your MySQL query statement below
-WITH t AS(
-    SELECT Wimbledon AS winner_id 
-    FROM Championships
-
-    UNION ALL
-
-    SELECT Fr_open
-    FROM Championships
-
-    UNION ALL
-
-    SELECT US_open
-    FROM Championships
-
-    UNION ALL
-
-    SELECT Au_open
-    FROM Championships
+# | player_id | player_name | grand_slams_count |
+WITH t AS(SELECT Wimbledon AS winner
+        FROM Championships
+        UNION ALL
+        SELECT Fr_open 
+        FROM Championships
+        UNION ALL
+        SELECT US_open 
+        FROM Championships
+        UNION ALL
+        SELECT AU_open 
+        FROM Championships
+), a AS(
+    SELECT winner, COUNT(winner) AS grand_slams_count
+    FROM t
+    GROUP BY winner
 )
-SELECT player_id,
-       player_name,
-       COUNT(*) AS grand_slams_count
+SELECT player_id, player_name, grand_slams_count
 FROM Players p
-JOIN t
-ON p.player_id = t.winner_id
-GROUP BY p.player_id
+JOIN a
+ON p.player_id = a.winner;
