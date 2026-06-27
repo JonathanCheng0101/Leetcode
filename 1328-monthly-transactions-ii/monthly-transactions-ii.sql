bf08dcf12f8f5395ed028aaaed6b75903a22997c
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-27 13:49:48 -0600

    Time: 687 ms (55.71%) | Memory: 0B (100.00%) - LeetSync

--- 1328-monthly-transactions-ii/monthly-transactions-ii.sql
+++ 1328-monthly-transactions-ii/monthly-transactions-ii.sql
@@ -1,41 +1,38 @@
-# Write your MySQL query statement below
+# month   | country | approved_count | approved_amount | chargeback_count | chargeback_amount 
+-- create cross table
 WITH c AS(
+        SELECT DATE_FORMAT(trans_date, "%Y-%m") AS `month`,
+       country, 
+       SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+       SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_amount
+FROM Transactions
+WHERE state = 'approved'
+GROUP BY country, DATE_FORMAT(trans_date, "%Y-%m")
+),
+ d AS(
     SELECT DATE_FORMAT(c.trans_date, "%Y-%m") AS `month`,
-       t.country,
-       COUNT(*) AS chargeback_count,
+         t.country,
+       COUNT(t.amount) AS chargeback_count,
        SUM(t.amount) AS chargeback_amount
-       
-    FROM Chargebacks c
-    JOIN Transactions t
-    ON c.trans_id = t.id
-    GROUP BY `month`, t.country
-), t AS(
-    SELECT DATE_FORMAT(trans_date, "%Y-%m") AS `month`,
-       country,
-       COUNT(*) AS approved_count,
-       SUM(amount) AS approved_amount  
-    FROM Transactions
-    WHERE state = 'approved'
-    GROUP BY `month`, country
-),tp AS(
-    SELECT `month`, country 
-    FROM t
-
-    UNION 
-
-    SELECT `month`, country 
-    FROM c
-)
-SELECT tp.`month`,
-        tp.country,
-        IFNULL(t.approved_count, 0) AS approved_count,
-        IFNULL(t.approved_amount, 0) AS approved_amount,
-        IFNULL(c.chargeback_count, 0)AS chargeback_count,
-        IFNULL(c.chargeback_amount, 0) AS chargeback_amount
-FROM tp
-LEFT JOIN t
-ON t.`month` = tp.`month` AND t.country = tp.country
+FROM Chargebacks c
+JOIN Transactions t
+ON c.trans_id = t.id
+GROUP BY t.country, DATE_FORMAT(c.trans_date, "%Y-%m")
+), a AS(
+    SELECT `month`, country
+FROM c
+UNION 
+SELECT `month`, country
+FROM d
+), e AS(
+SELECT a.`month`, a.country, IFNULL(c.approved_count, 0) AS approved_count, IFNULL(c.approved_amount, 0) AS approved_amount, IFNULL(d.chargeback_count, 0) AS chargeback_count, IFNULL(d.chargeback_amount, 0) AS chargeback_amount
+FROM a
 LEFT JOIN c
-ON c.`month` = tp.`month`  AND c.country = tp.country
+ON (a.`month` = c.`month`) AND  (a.country = c.country)
+LEFT JOIN d
+ON (a.`month` = d.`month`) AND  (a.country = d.country)
+)
 
-
+SELECT *
+FROM e
+WHERE approved_count IS NOT NULL AND chargeback_amount IS NOT NULL
