6b0d04d4d4d6305fde2730f536727af7727a77ce
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-29 18:08:46 -0600

    Time: 650 ms (87.13%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,11 +1,10 @@
 WITH t AS(
     SELECT user_id,
-           ROUND(SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END)/COUNT(*), 2) AS personal_avg
+        ROUND(SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END)/ COUNT(user_id), 2) AS confirmation_rate
     FROM Confirmations
     GROUP BY user_id
 )
-SELECT s.user_id,
-       IFNULL(t.personal_avg, 0) AS confirmation_rate
+SELECT s.user_id, IFNULL(t.confirmation_rate, 0) AS confirmation_rate
 FROM Signups s
 LEFT JOIN t
-ON s.user_id = t.user_id
+ON s.user_id = t.user_id;
