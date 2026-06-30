19a73fb1d938e81406f8f603d3a9087ad27dfec7
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-06-30 15:18:24 -0600

    Time: 709 ms (63.78%) | Memory: 0B (100.00%) - LeetSync

--- 1702-bank-account-summary/bank-account-summary.sql
+++ 1702-bank-account-summary/bank-account-summary.sql
@@ -1,21 +1,23 @@
 # Write your MySQL query statement below
+# user_id, user_name, credit, credit_limit_breached
+
 WITH deduct AS(
-    SELECT paid_by,
-           SUM(amount) AS amount
+    SELECT paid_by AS user_id,
+       SUM(amount) AS amount
     FROM Transactions
     GROUP BY paid_by
-), adding AS(
-    SELECT paid_to,
-           SUM(amount) AS amount
+), ad AS(
+    SELECT paid_to AS user_id,
+       SUM(amount) AS amount
     FROM Transactions
     GROUP BY paid_to
 )
 SELECT u.user_id,
        u.user_name,
-       u.credit - IFNULL(d.amount, 0) + IFNULL(a.amount, 0) AS credit,
-       CASE WHEN (u.credit - IFNULL(d.amount, 0) + IFNULL(a.amount, 0)) > 0 THEN "No" ELSE "Yes" END AS credit_limit_breached
+      (u.credit - IFNULL(deduct.amount, 0) + IFNULL(ad.amount, 0)) AS credit,
+      CASE WHEN (u.credit - IFNULL(deduct.amount, 0) + IFNULL(ad.amount, 0)) < 0 THEN 'Yes' ELSE 'No' END AS credit_limit_breached
 FROM Users u
-LEFT JOIN deduct d
-ON u.user_id = d.paid_by
-LEFT JOIN adding a
-ON u.user_id = a.paid_to
+LEFT JOIN deduct
+ON u.user_id = deduct.user_id
+LEFT JOIN ad
+ON u.user_id = ad.user_id
