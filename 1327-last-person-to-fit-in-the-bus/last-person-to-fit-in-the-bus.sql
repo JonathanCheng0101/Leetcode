ebab4059cf00d974453931443852d602cf7a1413
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-01 14:18:08 -0600

    Time: 1032 ms (25.75%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,10 @@
-# Write your MySQL query statement below
 WITH t AS(
-    SELECt *,
-           SUM(weight)OVER(ORDER BY turn ASC) AS total
+    SELECT *,
+        SUM(weight)OVER(ORDER BY turn ASC) AS total_weight
     FROM Queue
 )
 SELECT person_name
 FROM t
-WHERE total <= 1000
-ORDER BY total DESC
+WHERE total_weight <= 1000
+ORDER BY total_weight DESC
 LIMIT 1;
