6a44245e7fb3b7625f88766ce0504b619cef1fff
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-07 12:47:33 -0600

    Time: 381 ms (19.00%) | Memory: 0B (100.00%) - LeetSync

--- 1578-apples-oranges/apples-oranges.sql
+++ 1578-apples-oranges/apples-oranges.sql
@@ -1,5 +1,16 @@
-SELECT DISTINCT sale_date,
-       SUM(CASE WHEN fruit = 'apples' THEN sold_num ELSE 0 END)OVER(PARTITION BY sale_date)      
-       -  
-       SUM(CASE WHEN fruit = 'oranges' THEN sold_num ELSE 0 END)OVER(PARTITION BY sale_date) AS diff
-FROM Sales
+# | sale_date  | diff         |
+WITH t AS(
+    SELECT sale_date,
+        CASE WHEN fruit = 'apples' THEN sold_num END AS 'apples',
+        CASE WHEN fruit = 'oranges' THEN sold_num END AS 'oranges'
+    FROM Sales
+), a AS(
+    SELECT sale_date, MAX(apples) AS apples, MAX(oranges) AS oranges
+    FROM t
+    GROUP BY sale_date
+)
+SELECT sale_date,
+       (apples - oranges) AS diff
+FROM a
+ORDER BY sale_date ASC;
+
