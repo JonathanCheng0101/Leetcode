4597be838921e909c279c13db8fa38cba958f01a
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-09 18:25:06 -0600

    Time: 294 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 1578-apples-oranges/apples-oranges.sql
+++ 1578-apples-oranges/apples-oranges.sql
@@ -1,16 +1,14 @@
-# | sale_date  | diff         |
+#  sale_date  | diff 
 WITH t AS(
     SELECT sale_date,
-        CASE WHEN fruit = 'apples' THEN sold_num END AS 'apples',
-        CASE WHEN fruit = 'oranges' THEN sold_num END AS 'oranges'
+       CASE WHEN fruit = 'apples' THEN sold_num ELSE 0  END AS apples,
+       CASE WHEN fruit = 'oranges' THEN sold_num ELSE 0 END AS oranges
     FROM Sales
 ), a AS(
     SELECT sale_date, MAX(apples) AS apples, MAX(oranges) AS oranges
     FROM t
     GROUP BY sale_date
 )
-SELECT sale_date,
-       (apples - oranges) AS diff
+SELECT sale_date, apples - oranges AS diff
 FROM a
 ORDER BY sale_date ASC;
-
