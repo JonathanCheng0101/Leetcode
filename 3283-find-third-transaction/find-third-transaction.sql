0e0e6bf6d4115d523022a068dd0d3e7c71eb526a
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-16 11:56:02 -0600

    Time: 676 ms (9.88%) | Memory: 0B (100.00%) - LeetSync

--- 3283-find-third-transaction/find-third-transaction.sql
+++ 3283-find-third-transaction/find-third-transaction.sql
@@ -1,19 +1,21 @@
 # Write your MySQL query statement below
+# user_id | third_transaction_spend | third_transaction_date 
 WITH t AS(
     SELECT *,
-        COUNT(user_id)OVER(PARTITION BY user_id) AS cnt
-    FROM Transactions
-), a AS(
-    SELECT *,
-        LAG(spend)OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS prev_spend,
-        LAG(spend, 2)OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS prev2_spend,
-        ROW_NUMBER()OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS rn
-    FROM t
-    WHERE cnt >=3
+            ROW_NUMBER()OVER(PARTITION BY user_id ORDER BY transaction_date ASC) AS rn
+    FROM Transactions            
 )
-SELECT user_id,
-       spend AS third_transaction_spend,
-       transaction_date AS third_transaction_date
-FROM a
-WHERE (rn = 3) AND (spend > prev_spend) AND (spend > prev2_spend) 
+, a AS(
+    SELECT *
+FROM t
+WHERE rn <= 3
+), b AS(
+    SELECT *,
+       ROW_NUMBER()OVER(PARTITION BY user_id ORDER BY spend DESC) AS rn_spend
+    FROM a
+
+)
+SELECT user_id, spend AS third_transaction_spend, transaction_date AS third_transaction_date
+FROM b
+WHERE rn = 3 AND rn_spend = 1
 ORDER BY user_id ASC;
