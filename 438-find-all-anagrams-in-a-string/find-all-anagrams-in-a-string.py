93b602c84c40b648e337d4498a50daf2881294c4
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-16 13:03:09 -0600

    Time: 178 ms (29.41%) | Memory: 20 MB (15.31%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -2,12 +2,11 @@ from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         res = []
-        p_cnt = Counter(p)
         n = len(p)
         l = 0
 
         window = Counter(s[:n])
-
+        p_cnt = Counter(p)
         if window == p_cnt:
             res.append(0)
 
@@ -16,20 +15,15 @@ class Solution:
                 window[s[i]] = 1
             else:
                 window[s[i]] += 1
-            
-            window[s[l]] -= 1
 
+            window[s[l]] -= 1
             if window[s[l]] == 0:
                 del window[s[l]]
             l += 1
-            
+
             if window == p_cnt:
-                res.append(l)
-
+                res.append(i - n + 1)
+        
         return res
-
             
 
-                
-        
-        
