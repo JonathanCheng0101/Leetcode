cd96a497767c427e62e29e5dd05a5a1010b78bc7
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-16 13:19:48 -0600

    Time: 2 ms (61.72%) | Memory: 19.3 MB (92.83%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -2,17 +2,21 @@ from heapq import heappop, heapify, heappush
 from collections import defaultdict
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        d = {}
         res = []
-        word_dict = defaultdict(int)
-
         for word in words:
-            word_dict[word] += 1
+            if word not in d:
+                d[word] = 1
+            else:
+                d[word] += 1
+        
+        sorted_d = sorted(d.items(), key = lambda x:(-x[1], x[0]))[:k]
+        
+        for (k, v) in sorted_d:
+            res.append(k)
+        return res
 
-        lst = sorted(word_dict.items(), key=lambda x:(-x[1],x[0]))
-        for i in range(k):
-            res.append(lst[i][0])
             
 
-        return res
             
         
