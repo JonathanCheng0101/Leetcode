077dd99290e8d0170d3d7c9fb115cb500ab03557
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-17 13:04:31 -0600

    Time: 3 ms (91.19%) | Memory: 19 MB (99.72%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,12 +1,27 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        def encode(w):
-            w_dict = {}
-            res = []
-            for i,ele in enumerate(w):
-                if ele not in w_dict:
-                    w_dict[ele] = i
-                res.append(w_dict[ele])
-            return res
+        if len(s) != len(t):
+            return False
+        
+        d = {}
+        d_t = {}
+        
+        n = len(s)
+        for i in range(n):
+            if s[i] not in d:
+                d[s[i]] = t[i]
+            
+            else:
+                if t[i] != d[s[i]]:
+                    return False
+            
+            if t[i] not in d_t:
+                d_t[t[i]] = s[i]
+            
+            else:
+                if s[i] != d_t[t[i]]:
+                    return False
+        
+        return True
 
-        return encode(s) == encode(t)
+        
