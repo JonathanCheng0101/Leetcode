385720c55ad09d94e0bf8b59cc4f1bbc137ff7c6
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-17 13:00:07 -0600

    Time: 7 ms (72.11%) | Memory: 23.2 MB (15.99%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,20 +1,19 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        sorted_s = sorted(intervals, key = lambda x: (x[0], x[1]))
         res = []
 
-        lst = sorted(intervals, key = lambda x: (x[0], x[1]))
-
-        for ele in lst:
+        for ele in sorted_s:
             if not res:
                 res.append(ele)
             else:
-                # cur = res[-1],  nxt = ele
-                # non overlapping
-                if res[-1][1] < ele[0]:
-                    res.append(ele)
-                else:
+                # check intervals
+             
+                if res[-1][1] >= ele[0]:
                     res[-1][1] = max(res[-1][1], ele[1])
-
+                
+                else:
+                    res.append(ele)
         
         return res
-                
+       
