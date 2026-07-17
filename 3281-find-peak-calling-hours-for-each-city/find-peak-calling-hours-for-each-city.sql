ce3cac5bae13109a9317db23b488f1b58f6d16e5
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-17 11:13:26 -0600

    Time: 608 ms (30.00%) | Memory: 0B (100.00%) - LeetSync

--- 3281-find-peak-calling-hours-for-each-city/find-peak-calling-hours-for-each-city.sql
+++ 3281-find-peak-calling-hours-for-each-city/find-peak-calling-hours-for-each-city.sql
@@ -1,17 +1,19 @@
 WITH t AS(
-    SELECT DISTINCT city,
-       HOUR(call_time) AS `hour`,
-       COUNT(city) OVER(PARTITION BY city,HOUR(call_time)) AS cnt
-    FROM calls
-)
-, a AS(
-    SELECT *,
-        RANK()OVER(PARTITION BY city ORDER BY cnt DESC) AS rn
+    SELECT city,
+      HOUR(call_time) AS calling_hr,
+      COUNT(*) AS cnt
+    FROM Calls
+    GROUP BY city, HOUR(call_time)
+), a AS(
+    SELECT city,
+       calling_hr AS peak_calling_hour,
+       cnt AS number_of_calls,
+       RANK() OVER (PARTITION BY city ORDER BY cnt DESC) AS rn
     FROM t
 )
 SELECT city,
-       `hour` AS peak_calling_hour,
-       cnt AS number_of_calls
+       peak_calling_hour,
+       number_of_calls
 FROM a
 WHERE rn = 1
-ORDER BY `hour` DESC, city DESC;
+ORDER BY peak_calling_hour DESC, city DESC;
