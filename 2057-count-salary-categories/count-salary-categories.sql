a4baed8d1e52285f20ef26ada33a6419a37cd55c
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-19 15:14:17 -0600

    Time: 1402 ms (99.32%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,33 +1,23 @@
-WITH template AS(
+WITH t AS (
     SELECT 'Low Salary' AS category
-    UNION all
-    SELECT 'High Salary'
-    UNION all
-    SELECT 'Average Salary'
-), t AS(
-    SELECT *,
-       CASE WHEN income < 20000 THEN 1 ELSE 0 END AS Low_Salary,
-       CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 ELSE 0 END AS Average_Salary,
-       CASE WHEN income > 50000 THEN 1 ELSE 0 END AS High_Salary
-    FROM Accounts
-), a AS(
-    SELECT SUM(Low_Salary) AS `Low Salary`,
-       SUM(Average_Salary) AS `Average Salary`,
-       SUM(High_Salary) AS `High Salary`
-    FROM t
+    UNION ALL
+    SELECT 'Average Salary' AS category
+    UNION ALL
+    SELECT 'High Salary' AS category
 )
-SELECT 'Low Salary' AS category,
-       `Low Salary` AS accounts_count
-FROM a
-
-UNION ALL
-
-SELECT 'Average Salary',
-        `Average Salary`
-FROM a
-
-UNION ALL
-
-SELECT 'High Salary',
-       `High Salary`
-FROM a
+, a AS(
+    SELECT *,
+       CASE WHEN income < 20000 THEN 'Low Salary'
+            WHEN income > 50000 THEN 'High Salary'
+            ELSE 'Average Salary' END AS status
+    FROM Accounts
+), b AS(
+    SELECT status, COUNT(status) AS accounts_count
+    FROM a
+    GROUP BY status
+)
+SELECT t.category,
+       IFNULL(b.accounts_count, 0) AS accounts_count
+FROM t
+LEFT JOIN b
+ON t.category = b.status;
