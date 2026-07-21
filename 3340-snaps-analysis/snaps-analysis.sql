04736640802ffc453e0f7ce9f797a4c18777a9f0
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-21 15:49:38 -0600

    Time: 610 ms (28.57%) | Memory: 0B (100.00%) - LeetSync

--- 3340-snaps-analysis/snaps-analysis.sql
+++ 3340-snaps-analysis/snaps-analysis.sql
@@ -1,12 +1,17 @@
-# Write your MySQL query statement below
+# | age_bucket | send_perc | open_perc 
 WITH t AS(
-    SELECT Age.age_bucket, a.user_id, a.activity_type, a.time_spent
+    SELECT age.age_bucket, a.user_id, a.activity_type, a.time_spent
     FROM Activities a
-    JOIN Age 
-    ON a.user_id = Age.user_id
-)
-SELECT age_bucket,
-       ROUND(100 * SUM(CASE WHEN activity_type = 'send' THEN time_spent ELSE 0 END)/ SUM(time_spent), 2) AS send_perc,
-       ROUND(100 * SUM(CASE WHEN activity_type = 'open' THEN time_spent ELSE 0 END)/ SUM(time_spent), 2) AS open_perc
+    JOIN Age age
+    ON a.user_id = age.user_id
+), a AS(
+    SELECT age_bucket,
+       SUM(CASE WHEN activity_type = 'send' THEN time_spent ELSE 0 END) AS send,
+       SUM(CASE WHEN activity_type = 'open' THEN time_spent ELSE 0 END) AS open,
+       SUM(time_spent) AS total
 FROM t
 GROUP BY age_bucket
+
+)
+SELECT age_bucket,  ROUND(100 * send/ total, 2) AS send_perc,  ROUND(100 *open/ total, 2) AS open_perc
+FROM a;
