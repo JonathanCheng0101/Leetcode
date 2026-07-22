84cec22e968aab4cba48ee7253748269d5a9b4e0
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-22 10:40:22 -0600

    Time: 272 ms (30.32%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,16 +1,14 @@
-# machine_id, process_time
+#  machine_id | processing_time
 WITH t AS(
-    SELECT a1.machine_id,
-       a1.process_id,
-       a1.`timestamp`AS start_time,
-       a2.`timestamp`AS end_time,
-       a2.`timestamp` - a1.`timestamp` AS total
-    FROM Activity a1
-    JOIN Activity a2
-    ON a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id
-    WHERE a1.activity_type = 'start' AND a2.activity_type = 'end'
+        SELECT machine_id, process_id, CASE WHEN activity_type = 'start' THEN `timestamp` ELSE 0 END AS start_time,
+        CASE WHEN activity_type = 'end' THEN `timestamp` ELSE 0 END AS end_time
+        FROM Activity
+        )
+, a AS(
+    SELECT machine_id, process_id, MAX(end_time) - MAX(start_time) AS time_used 
+    FROM t
+    GROUP BY machine_id, process_id
 )
-SELECT machine_id, ROUND(AVG(total), 3) AS processing_time
-FROM t
+SELECT machine_id, ROUND(AVG(time_used), 3) AS processing_time
+FROM a
 GROUP BY machine_id
-
