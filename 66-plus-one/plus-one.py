72f63fba66af6741b85b96d06e6caa3cb99a5ff7
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-22 11:26:57 -0600

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.08%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,10 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        total = 0
-        for i in range(len(digits)-1, -1, -1):
-            total += digits[i] * 10**(len(digits) - i - 1)
-
-        total += 1
-
-        total = [int(x) for x in str(total)]
-        return total
+        new_digit = (str(d) for d in digits)
+        num = int("".join(new_digit))
+        new_num = str(num + 1)
+        new_lst =  list(new_num)
+        return [int(num) for num in new_lst]
+       
