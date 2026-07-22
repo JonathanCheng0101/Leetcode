c88a776efbcba43fdec76f71320a6c8a4bd14bee
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-22 09:54:56 -0600

    Time: 530 ms (37.52%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,4 +1,8 @@
-SELECT player_id,
-       MIN(event_date) AS first_login
-FROM Activity
-GROUP BY player_id;
+WITH t AS(
+    SELECT *,
+       ROW_NUMBER()OVER(PARTITION BY player_id ORDER BY event_date ASC) AS rn
+    FROM Activity
+)
+SELECT player_id, event_date AS first_login
+FROM t
+WHERE rn = 1;
