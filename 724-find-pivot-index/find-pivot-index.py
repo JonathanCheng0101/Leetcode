d683c49da383de10d58f1459a3fb2db09889f186
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-22 11:21:56 -0600

    Time: 9 ms (34.13%) | Memory: 21 MB (6.24%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,15 +1,18 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         d = {0: -1} # prefix: index
-
+        sum_total = sum(nums)
         prefix = 0
-        lst_sum = sum(nums)
 
         for i, num in enumerate(nums):
-            if prefix == (lst_sum - prefix - num):
+            back = sum_total - prefix - num 
+            if back == prefix:
                 return i
             
             prefix += num
+            d[prefix] = i
         
         return -1
-            
+
+
+        
