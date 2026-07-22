8a4990da477cb956d04e78ef8e9a5a4c39618db7
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-22 10:34:17 -0600

    Time: 1528 ms (29.68%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,16 +1,18 @@
 WITH t AS(
-    SELECT visit_id
-    FROM Transactions
+    SELECT visit_id, SUM(amount) AS amount
+    FROM Transactions 
     GROUP BY visit_id
 ), a AS(
-    SELECT customer_id
-FROM Visits v
-WHERE NOT EXISTS(
-    SELECT 1
-    FROM t
-    WHERE v.visit_id = t.visit_id
-)
-)
-SELECT customer_id, COUNT(customer_id) AS count_no_trans
+    SELECT v.customer_id, t.visit_id
+    FROM Visits v
+    LEFT JOIN t
+    ON v.visit_id = t.visit_id
+), b AS(
+    SELECT customer_id, SUM(CASE WHEN visit_id IS NULL THEN 1 ELSE 0 END) AS count_no_trans
 FROM a
 GROUP BY customer_id
+
+)
+SELECT *
+FROM b
+WHERE count_no_trans != 0
