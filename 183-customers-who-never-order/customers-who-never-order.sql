f55b12158d44b6c5ec63218acad49202c341cb7c
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-22 09:51:16 -0600

    Time: 637 ms (23.48%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,7 +1,7 @@
-SELECT name AS Customers
+SELECT c.name AS Customers
 FROM Customers c
 WHERE NOT EXISTS(
     SELECT 1
     FROM Orders o
-    WHERE o.customerId = c.id
-);
+    WHERE c.id = o.customerId
+)
