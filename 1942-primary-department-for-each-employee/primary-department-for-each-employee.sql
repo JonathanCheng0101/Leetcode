15fc4b57f672acaff583cb015c0ee72c14555e16
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-23 10:29:18 -0600

    Time: 725 ms (11.06%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,8 +1,7 @@
 WITH t AS(
-    SELECT *,
-        COUNT(employee_id)OVER(PARTITION BY employee_id) AS cnt
+    SELECT *, COUNT(department_id)OVER(PARTITION BY employee_id) AS depart_cnt
     FROM Employee
 )
 SELECT employee_id, department_id
 FROM t
-WHERE cnt = 1 OR (cnt > 1 AND primary_flag = 'Y');
+WHERE depart_cnt = 1 OR primary_flag = 'Y';
