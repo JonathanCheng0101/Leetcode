96772243faf0d0d4ba20ea10eec738886b0f62a4
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-23 10:18:45 -0600

    Time: 630 ms (20.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1837-daily-leads-and-partners/daily-leads-and-partners.sql
@@ -0,0 +1,4 @@
+#  date_id   | make_name | unique_leads | unique_partners |
+SELECT date_id, make_name, COUNT(DISTINCT lead_id) AS unique_leads, COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY date_id, make_name;
