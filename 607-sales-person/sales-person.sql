52a380738e2b5c55a66d647d5de90e4898b78777
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-23 10:55:27 -0600

    Time: 1865 ms (9.42%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,10 +1,10 @@
+# name
 SELECT s.name
 FROM SalesPerson s
-WHERE s.sales_id NOT IN (
-    SELECT o.sales_id
-    FROM Orders o
-    JOIN Company c
-      ON o.com_id = c.com_id
-    WHERE c.name = 'RED'
-      AND o.sales_id IS NOT NULL
-);
+WHERE NOT EXISTS(
+    SELECT 1
+    FROM Company c
+    JOIN Orders o
+    ON c.com_id = o.com_id
+    WHERE o.sales_id = s.sales_id AND c.name = 'RED'
+)
