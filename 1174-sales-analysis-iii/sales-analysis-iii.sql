9c0de307b80e5d1fd22574e71afbe16b4a6f5073
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-07-26 18:13:37 -0600

    Time: 1093 ms (89.73%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,11 +1,21 @@
-SELECT DISTINCT p.product_id,
-       p.product_name
-FROM Product p
-JOIN Sales s
-ON s.product_id = p.product_id
+WITH t AS(
+    SELECT DISTINCT product_id
+    FROM Sales
+    WHERE sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+), a AS(
+    SELECT DISTINCT product_id
+    FROM Sales
+    WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'
+), b  AS(
+    SELECT t.product_id
+FROM t
 WHERE NOT EXISTS(
     SELECT 1
-    FROM Sales s
-    WHERE s.product_id = p.product_id AND
-          (('2019-01-01'> s.sale_date) OR ('2019-03-31'< s.sale_date))
-)  AND s.sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+    FROM a
+    WHERE t.product_id = a.product_id
+)
+)
+SELECT b.product_id, p.product_name
+FROM b
+JOIN Product p
+ON b.product_id = p.product_id;
