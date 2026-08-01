1e2e98c586ff844ddaa0290d822f389666ee70e5
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-08-01 11:58:00 -0600

    Time: 0 ms (100.00%) | Memory: 20.3 MB (34.23%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = set()
+
+        def dfs(node):
+            if node in visited:
+                return 
+            
+            visited.add(node)
+            
+            for key in rooms[node]:
+                dfs(key)
+
+        
+        dfs(0)
+
+        return len(visited) == len(rooms)
+        
