baf79bcc65f165294119cb2ac94899a28afedafd
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-08-02 14:23:06 -0600

    Time: 365 ms (24.53%) | Memory: 0B (100.00%) - LeetSync

--- 3339-top-percentile-fraud/top-percentile-fraud.sql
+++ 3339-top-percentile-fraud/top-percentile-fraud.sql
@@ -1,11 +1,12 @@
-# Write your MySQL query statement below
 WITH t AS(
     SELECT *,
-        RANK()OVER(PARTITION BY state ORDER BY fraud_score DESC) AS rn,
-        CEIL(COUNT(state)OVER(PARTITION BY state) * 0.05) AS cnt
+        COUNT(policy_id) OVER(PARTITION BY state) AS total_number,
+        RANK()OVER(PARTITION BY state ORDER BY fraud_score DESC) AS rn
     FROM Fraud
 )
-SELECT policy_id, state, fraud_score
+SELECT policy_id,
+       state,
+       fraud_score
 FROM t
-WHERE rn <= cnt
+WHERE rn <= CEIL(total_number * 0.05)
 ORDER BY state ASC, fraud_score DESC, policy_id ASC;
