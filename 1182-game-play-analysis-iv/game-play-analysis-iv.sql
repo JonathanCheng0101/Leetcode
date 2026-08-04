f03f64460f75ea0e781717a971b612d34698f30f
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-08-04 13:38:30 -0600

    Time: 642 ms (32.58%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,8 +1,17 @@
-# Write your MySQL query statement below
-WITH t AS(
-    SELECT *,
-           MIN(event_date)OVER(PARTITION BY player_id) AS first_date
+WITH t AS (
+    SELECT COUNT(DISTINCT player_id) AS total_cnt
     FROM Activity
+), a AS(
+    SELECT *,
+       LEAD(event_date) OVER(PARTITION BY player_id ORDER BY event_date ASC) AS nxt_date,
+       ROW_NUMBER()OVER(PARTITION BY player_id ORDER BY event_date ASC) AS rn
+    FROM Activity
+), b AS(
+    SELECT COUNT(player_id) AS qualified
+    FROM a
+    WHERE  rn = 1 AND DATEDIFF(nxt_date, event_date) = 1
 )
-SELECT ROUND(SUM(CASE WHEN DATEDIFF(event_date,first_date) = 1 THEN 1 ELSE 0 END)/COUNT(DISTINCT player_id),2) AS fraction
-FROM t;
+SELECT ROUND(b.qualified/ t.total_cnt, 2) AS fraction
+FROM b
+CROSS JOIN t
+
