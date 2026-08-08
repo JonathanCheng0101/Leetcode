4ebbd9d8f23eac919a48a09e07f01bc53927a5df
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-08-08 15:35:28 -0600

    Time: 1379 ms (45.67%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,10 +1,13 @@
-# Write your MySQL query statement below
-SELECT  u.user_id AS buyer_id,
+WITH t AS(
+    SELECT buyer_id,
+       COUNT(buyer_id) AS cnt
+    FROM Orders
+    WHERE order_date >= '2019-01-01' AND order_date < '2020-01-01'
+    GROUP BY buyer_id
+)
+SELECT u.user_id AS buyer_id,
        u.join_date,
-       COUNT(o.item_id) AS orders_in_2019
+       IFNULL(t.cnt, 0) AS orders_in_2019
 FROM Users u
-LEFT JOIN Orders o
-ON u.user_id = o.buyer_id AND DATE_FORMAT(o.order_date, "%Y") = '2019' 
-GROUP BY u.user_id
-
-
+LEFT JOIN t
+ON u.user_id = t.buyer_id;
