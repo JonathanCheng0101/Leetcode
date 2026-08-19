1de16c671871a85feaf3a6f09473e16f7bc5763d
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-08-19 16:44:30 -0600

    Time: 23 ms (98.42%) | Memory: 29.5 MB (96.43%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -5,59 +5,57 @@ class Node:
         self.prev = None
         self.next = None
 
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
+        self.storage = {}
 
         self.head = Node()
         self.tail = Node()
+
         self.head.next = self.tail
         self.tail.prev = self.head
 
-
-    def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-
-        node = self.cache[key]
-
-        # 把 node 從原本位置拔掉
+    def remove(self, node):
         node.prev.next = node.next
         node.next.prev = node.prev
 
-        # 放到最前面
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
+    def add_to_end(self, node):
+        prev_node = self.tail.prev
+
+        prev_node.next = node
+        node.prev = prev_node
+
+        node.next = self.tail
+        self.tail.prev = node
+
+    def get(self, key: int) -> int:
+        if key not in self.storage:
+            return -1
+
+        node = self.storage[key]
+
+        self.remove(node)
+        self.add_to_end(node)
 
         return node.value
 
-
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            # 已存在：先拔掉舊 node
-            node = self.cache[key]
-            node.prev.next = node.next
-            node.next.prev = node.prev
+        if key in self.storage:
+            node = self.storage[key]
             node.value = value
 
+            self.remove(node)
+            self.add_to_end(node)
+
         else:
-            # 不存在：建立新 node
+            if len(self.storage) == self.capacity:
+                oldest = self.head.next
+                self.remove(oldest)
+                del self.storage[oldest.key]
+
             node = Node(key, value)
-            self.cache[key] = node
-
-        # 放到最前面
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
-
-        # 超過容量 → 刪掉最後一個
-        if len(self.cache) > self.capacity:
-            lru = self.tail.prev
-            lru.prev.next = self.tail
-            self.tail.prev = lru.prev
-            del self.cache[lru.key]
+            self.storage[key] = node
+            self.add_to_end(node)
