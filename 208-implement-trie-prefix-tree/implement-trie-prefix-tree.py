6151683ea668c427bf1a36433909ad4d0ff717bd
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-08-22 14:59:50 -0600

    Time: 31 ms (76.65%) | Memory: 31.4 MB (95.65%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,39 +1,36 @@
 class Trie:
 
     def __init__(self):
-        self.trie = {}
-        
+        self.trie = {}        
 
     def insert(self, word: str) -> None:
         d = self.trie
-        for char in word:
-            if char not in d:
-                d[char] = {}
+        for c in word:
+            if c not in d:
+                d[c] = {}
             
-            d = d[char]
+            d = d[c]
         
         d['.'] = '.'
         
+        
+
     def search(self, word: str) -> bool:
         d = self.trie
         for c in word:
             if c not in d:
                 return False
-            
             d = d[c]
-        
-        return '.' in d
-        
+        return '.' in d       
 
     def startsWith(self, prefix: str) -> bool:
         d = self.trie
         for c in prefix:
             if c not in d:
                 return False
-            
             d = d[c]
-        
-        return True
+        return True  
+
         
 
 
