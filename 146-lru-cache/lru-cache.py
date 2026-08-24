a1aa457365294481491bc75e696d2b5fd1863578
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-08-24 11:08:35 -0600

    Time: 26 ms (94.92%) | Memory: 29.1 MB (98.93%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,9 +1,9 @@
 class Node:
-    def __init__(self, key= 0, value= 0):
+    def __init__(self, key = 0, value = 0):
         self.key = key
-        self.value = value
+        self.val = value
         self.prev = None
-        self.next = None
+        self.nxt = None
 
 class LRUCache:
 
@@ -11,62 +11,61 @@ class LRUCache:
         self.capacity = capacity
         self.cache = {}
 
-        self.head = Node()
-        self.tail = Node()
+        self.left = Node()
+        self.right = Node()
 
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    # 把 node 從 Linked List 原本的位置拆掉
+        self.left.next = self.right
+        self.right.prev = self.left
+    
     def remove(self, node):
         prev_node = node.prev
-        next_node = node.next
+        nxt_node = node.next
 
-        prev_node.next = next_node
-        next_node.prev = prev_node
+        prev_node.next = nxt_node
+        nxt_node.prev = prev_node
     
-    # 把 node 插到 tail 前面，成為最新使用
-    def insert(self, node):
-        prev_node = self.tail.prev
+    def add_to_end(self, node):
+        prev_node = self.right.prev
         
         prev_node.next = node
         node.prev = prev_node
 
-        node.next = self.tail
-        self.tail.prev = node
+        node.next = self.right
+        self.right.prev = node        
 
     def get(self, key: int) -> int:
-        res = -1
-        if key in self.cache:
-            node = self.cache[key]
-
-
-            # remove old
-            self.remove(node)
-            # insert 
-            self.insert(node)
-            res = node.value
+        if key not in self.cache:
+            return -1
+        
+        # cache hit --> update linked list
+        node = self.cache[key]
+        res = node.val
+        self.remove(node)
+        self.add_to_end(node)
 
         return res
-
+        
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
-            node.value = value
+            node.val = value
 
-            self.remove(node) 
-            self.insert(node)
+            self.remove(node)
+            self.add_to_end(node)
         
         else:
             node = Node(key, value)
             self.cache[key] = node
-            self.insert(node)
+            self.add_to_end(node)
 
             if len(self.cache) > self.capacity:
-                headnode = self.head.next
-                self.remove(headnode)
+                lru = self.left.next
+                self.remove(lru)
+                del self.cache[lru.key]
 
-                del self.cache[headnode.key]
+
+        
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
