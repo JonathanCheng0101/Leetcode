5c29c67652595383b2b7e98b61f3b11773cddf97
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-08-24 10:34:51 -0600

    Time: 4 ms (76.64%) | Memory: 20.5 MB (5.84%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.py
+++ 860-design-circular-queue/design-circular-queue.py
@@ -1,25 +1,35 @@
 class ListNode:
-    def __init__(self, val, nxt, prev):
+    def __init__(self, val):
         self.val = val
-        self.next = nxt
-        self.prev = prev
+        self.next = None
+        self.prev = None
+
 
 class MyCircularQueue:
 
     def __init__(self, k: int):
         self.space = k
-        self.left = ListNode(0, None, None)
-        self.right = ListNode(0, None, self.left)
+        self.size = 0
+
+        self.left = ListNode(0)
+        self.right = ListNode(0)
         self.left.next = self.right
-        
+        self.right.prev = self.left
 
     def enQueue(self, value: int) -> bool:
-        if self.space == 0:
+        if self.isFull():
             return False
-        cur = ListNode(value, self.right, self.right.prev)
-        self.right.prev.next = cur
-        self.right.prev = cur
-        self.space -= 1       
+        
+        node = ListNode(value)
+        next_node = self.left.next
+
+        self.left.next = node
+        node.prev = self.left
+
+        node.next = next_node
+        next_node.prev = node   
+
+        self.space -= 1     
 
         return True        
 
@@ -27,28 +37,33 @@ class MyCircularQueue:
         if self.isEmpty():
             return False
         
-        self.left.next = self.left.next.next
-        self.left.next.prev = self.left
-        self.space += 1
-        
+        cur = self.right.prev
+        prev_node = self.right.prev.prev
+
+        self.right.prev = prev_node
+        prev_node.next = self.right   
+
+        self.space += 1     
+
         return True        
 
     def Front(self) -> int:
         if self.isEmpty():
             return -1
-        return self.left.next.val
+        
+        return self.right.prev.val
         
     def Rear(self) -> int:
         if self.isEmpty():
             return -1
-        return self.right.prev.val
         
+        return self.left.next.val        
+
     def isEmpty(self) -> bool:
         return self.left.next == self.right
         
     def isFull(self) -> bool:
-        return self.space == 0   
-
+        return self.space == 0
 
 # Your MyCircularQueue object will be instantiated and called as such:
 # obj = MyCircularQueue(k)
