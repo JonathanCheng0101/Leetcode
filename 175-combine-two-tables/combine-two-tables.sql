f3ab930e2ca51cd2a7d2023757cd2666e9a04b8f
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-02 14:00:14 -0600

    Time: 484 ms (34.28%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,8 @@
-# | firstName | lastName | city          | state 
-SELECT p.firstName, p.lastName, a.city, a.state
+SELECT firstName,
+       lastName,
+       city,
+       state
 FROM Person p
 LEFT JOIN Address a
 ON p.personId = a.personId;
+ 
