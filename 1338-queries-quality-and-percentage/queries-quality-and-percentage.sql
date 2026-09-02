42288620aac5d7ac23fd2aeabd6d151756b3a65a
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-02 16:32:36 -0600

    Time: 372 ms (73.63%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,6 +1,13 @@
-#  query_name | quality | poor_query_percentage 
+WITH t AS(
+    SELECT query_name,
+        result,
+        rating /`position` AS quality,
+        CASE WHEN  rating < 3 THEN 1 ELSE 0 END AS poor_quality
+    FROM Queries
+)
+
 SELECT query_name,
-       ROUND(AVG(rating/ `position`), 2) AS quality,
-       ROUND(100 * SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)/COUNT(*), 2) AS poor_query_percentage
-FROM Queries
-GROUP BY query_name
+       ROUND(AVG(quality), 2) AS quality,
+       ROUND(100 * SUM(poor_quality)/COUNT(*), 2) AS poor_query_percentage 
+FROM t
+GROUP BY query_name;
