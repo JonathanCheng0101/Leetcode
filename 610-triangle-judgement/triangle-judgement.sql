6929967bcac46731e504a437cc5ef824a706b707
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-02 15:55:08 -0600

    Time: 333 ms (39.69%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,3 +1,3 @@
 SELECT *,
-       CASE WHEN (x+ y > z )AND(y+ z > x) AND (x+ z >y) THEN 'Yes'ELSE 'No' END AS triangle
+       CASE WHEN (x + y > z) AND (y + z > x) AND (x + z > y) THEN 'Yes' ELSE 'No' END AS triangle
 FROM Triangle
