f39b70459e637785e551cef32a6fccab581a868e
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-02 20:02:03 -0600

    Time: 306 ms (24.25%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,14 +1,10 @@
-#  machine_id | processing_time
+# Write your MySQL query statement below
 WITH t AS(
-        SELECT machine_id, process_id, CASE WHEN activity_type = 'start' THEN `timestamp` ELSE 0 END AS start_time,
-        CASE WHEN activity_type = 'end' THEN `timestamp` ELSE 0 END AS end_time
-        FROM Activity
-        )
-, a AS(
-    SELECT machine_id, process_id, MAX(end_time) - MAX(start_time) AS time_used 
-    FROM t
-    GROUP BY machine_id, process_id
+    SELECT *,
+        CASE WHEN activity_type = 'start' THEN -`timestamp` ELSE  `timestamp` END AS time_accu,
+        CASE WHEN activity_type = 'start' THEN 1 ELSE 0 END AS time_chk
+    FROM Activity
 )
-SELECT machine_id, ROUND(AVG(time_used), 3) AS processing_time
-FROM a
-GROUP BY machine_id
+SELECT machine_id, ROUND(SUM(time_accu)/ SUM(time_chk), 3) AS processing_time 
+FROM t
+GROUP BY machine_id;
