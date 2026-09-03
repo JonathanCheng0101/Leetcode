b938d79ae3ef2c561c2ed138ecfdc0b03fa42319
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-02 19:29:09 -0600

    Time: 1074 ms (61.51%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,14 +1,15 @@
-WITH t AS(
+# Write your MySQL query statement below
+WITH t AS (
     SELECT s.student_id, s.student_name, sub.subject_name
     FROM Students s
     CROSS JOIN Subjects sub
 ), a AS(
-    SELECT student_id, subject_name, COUNT(*) AS attended_exams
+    SELECT student_id, subject_name, COUNT(*) AS cnt
     FROM Examinations
     GROUP BY student_id, subject_name
 )
-SELECT t.student_id, t.student_name, t.subject_name, IFNULL(a.attended_exams, 0) AS attended_exams
+SELECT t.student_id, t.student_name, t.subject_name, IFNULL(a.cnt, 0) AS attended_exams
 FROM t
-LEFT JOIN a 
+LEFT JOIN a
 ON t.student_id = a.student_id AND t.subject_name = a.subject_name
-ORDER BY t.student_id ASC, t.subject_name ASC;
+ORDER BY student_id ASC, subject_name ASC;
