25f60da636dbca048f6453f8ee82960feb6e4049
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-03 15:37:34 -0600

    Time: 497 ms (94.38%) | Memory: 0B (100.00%) - LeetSync

--- 1892-find-total-time-spent-by-each-employee/find-total-time-spent-by-each-employee.sql
+++ 1892-find-total-time-spent-by-each-employee/find-total-time-spent-by-each-employee.sql
@@ -1,9 +1,5 @@
-WITH t AS(
-    SELECT emp_id, event_day, out_time - in_time AS duration
-    FROM Employees
-)
 SELECT event_day AS `day`,
        emp_id,
-       SUM(duration) AS total_time
-FROM t
-GROUP BY emp_id, event_day;
+       SUM(out_time) - SUM(in_time) As total_time
+FROM Employees
+GROUP BY event_day, emp_id
