29813444afed1300665c47058c04da50851eccd3
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-03 15:08:39 -0600

    Time: 615 ms (32.81%) | Memory: 0B (100.00%) - LeetSync

--- 1301-reformat-department-table/reformat-department-table.sql
+++ 1301-reformat-department-table/reformat-department-table.sql
@@ -1,15 +1,16 @@
 SELECT id,
-       MAX(CASE WHEN  `month` = 'JAN' THEN revenue END) AS Jan_Revenue,
-       MAX(CASE WHEN  `month` = 'Feb' THEN revenue END) AS Feb_Revenue,
-       MAX(CASE WHEN  `month` = 'Mar' THEN revenue END) AS Mar_Revenue,
-       MAX(CASE WHEN  `month` = 'Apr' THEN revenue END) AS Apr_Revenue,
-       MAX(CASE WHEN  `month` = 'May' THEN revenue END) AS May_Revenue,
-       MAX(CASE WHEN  `month` = 'Jun' THEN revenue END) AS Jun_Revenue,
-       MAX(CASE WHEN  `month` = 'Jul' THEN revenue END) AS Jul_Revenue,
-       MAX(CASE WHEN  `month` = 'Aug' THEN revenue END) AS Aug_Revenue,
-       MAX(CASE WHEN  `month` = 'Sep' THEN revenue END) AS Sep_Revenue,
-       MAX(CASE WHEN  `month` = 'Oct' THEN revenue END) AS Oct_Revenue,
-       MAX(CASE WHEN  `month` = 'Nov' THEN revenue END) AS Nov_Revenue,
-       MAX(CASE WHEN  `month` = 'Dec' THEN revenue END) AS Dec_Revenue
+       MAX(CASE WHEN `month` = 'Jan' THEN revenue ELSE null END) AS Jan_Revenue,
+       MAX(CASE WHEN `month` = 'Feb' THEN revenue ELSE null END) AS Feb_Revenue,
+       MAX(CASE WHEN `month` = 'Mar' THEN revenue ELSE null END) AS Mar_Revenue,
+       MAX(CASE WHEN `month` = 'Apr' THEN revenue ELSE null END) AS Apr_Revenue,
+       MAX(CASE WHEN `month` = 'May' THEN revenue ELSE null END) AS May_Revenue,
+       MAX(CASE WHEN `month` = 'Jun' THEN revenue ELSE null END) AS Jun_Revenue,
+       MAX(CASE WHEN `month` = 'Jul' THEN revenue ELSE null END) AS Jul_Revenue,
+       MAX(CASE WHEN `month` = 'Aug' THEN revenue ELSE null END) AS Aug_Revenue,
+       MAX(CASE WHEN `month` = 'Sep' THEN revenue ELSE null END) AS Sep_Revenue,
+       MAX(CASE WHEN `month` = 'Oct' THEN revenue ELSE null END) AS Oct_Revenue,
+       MAX(CASE WHEN `month` = 'Nov' THEN revenue ELSE null END) AS Nov_Revenue,
+       MAX(CASE WHEN `month` = 'Dec' THEN revenue ELSE null END) AS Dec_Revenue
 FROM Department
 GROUP BY id
+ORDER BY id ASC
