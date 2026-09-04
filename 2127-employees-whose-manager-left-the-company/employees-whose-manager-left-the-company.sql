0ec97fad9ca1950936b52a36fbb870b0d6574342
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-04 15:10:41 -0600

    Time: 334 ms (88.79%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,8 +1,9 @@
+# Write your MySQL query statement below
 SELECT e1.employee_id
 FROM Employees e1
-WHERE NOT EXISTS(
+WHERE e1.salary < 30000 AND e1.manager_id IS NOT NULL AND NOT EXISTS(
     SELECT 1
     FROM Employees e2
-    WHERE e1.manager_id = e2.employee_id OR e1.manager_id IS NULL
-) AND e1.salary < 30000
+    WHERE e1.manager_id = e2.employee_id
+)
 ORDER BY e1.employee_id;
