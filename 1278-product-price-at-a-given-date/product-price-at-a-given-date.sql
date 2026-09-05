433bc3b7f3ecf96a33a5bbbc9634820a3df4c739
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-05 11:17:00 -0600

    Time: 749 ms (20.84%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,16 +1,16 @@
 WITH t AS(
-    SELECT DISTINCT product_id, 10 AS price
+    SELECT *,
+        ROW_NUMBER()OVER(PARTITION BY product_id ORDER BY change_date DESC) AS rn
     FROM Products
+    WHERE change_date <= '2019-08-16' 
 ), a AS(
-    SELECT product_id, new_price, ROW_NUMBER()OVER(PARTITION BY product_id ORDER BY change_date DESC) AS rn
-    FROM Products
-    WHERE change_date <= '2019-08-16'
-), b AS(
     SELECT product_id, new_price
-    FROM a
-    WHERE rn = 1
+FROM t
+WHERE rn = 1
+
 )
-SELECT t.product_id, IFNULL(b.new_price, t.price) AS price
-FROM t 
-LEFT JOIN b
-ON t.product_id = b.product_id
+
+SELECT DISTINCT p.product_id, IFNULL(a.new_price, 10) AS price
+FROM Products p
+LEFT JOIN a
+ON p.product_id = a.product_id;
