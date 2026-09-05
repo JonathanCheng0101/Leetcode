379ad355f948eea15ef92ca887c560e68e4f2b19
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-05 10:36:07 -0600

    Time: 819 ms (11.72%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,9 +1,8 @@
-# Write your MySQL query statement below
 WITH t AS(
     SELECT num,
-        CASE WHEN (LEAD(num)OVER(ORDER BY id ASC) = num) AND (LAG(num)OVER(ORDER BY id ASC) = num) THEN 1 END AS chk
+        CASE WHEN LAG(num)OVER(ORDER BY id ASC) = num AND LEAD(num)OVER(ORDER BY id ASC) = num THEN 1 ELSE 0 END AS chk
     FROM Logs
 )
 SELECT DISTINCT num AS ConsecutiveNums
 FROM t
-WHERE chk = 1
+WHERE chk = 1;
