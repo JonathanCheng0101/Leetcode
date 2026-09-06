826c83e732e216a7dccb1abb1317b205ae176159
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-06 11:44:06 -0600

    Time: 1404 ms (5.06%) | Memory: 0B (100.00%) - LeetSync

--- 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
+++ 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
@@ -1,23 +1,21 @@
 WITH t AS(
-    SELECT user_id
+    SELECT user_id,reaction, COUNT(*) AS reaction_cnt
+    FROM reactions
+    GROUP BY user_id, reaction
+), a AS(
+    SELECT *, RANK()OVER(PARTITION BY user_id ORDER BY reaction_cnt DESC) AS rn
+    FROM t
+), b AS(
+    SELECT user_id, COUNT(content_id) AS total
     FROM reactions
     GROUP BY user_id
-    HAVING COUNT(DISTINCT content_id) >= 5
-),
-a AS(
-    SELECT r.user_id,
-        r.reaction,
-        COUNT(*) AS reaction_cnt,
-        COUNT(*)/ SUM(COUNT(*))OVER(PARTITION BY user_id) AS reaction_ratio
-    FROM reactions r
-    JOIN t
-    ON r.user_id = t.user_id
-    GROUP BY r.user_id, r.reaction
+    HAVING COUNT(content_id) >= 5
 )
-SELECT user_id,
-       reaction AS dominant_reaction,
-       ROUND(reaction_ratio, 2) AS reaction_ratio
-FROM a
-WHERE reaction_ratio >= 0.6
-ORDER BY reaction_ratio DESC, user_id ASC;
-
+SELECT b.user_id,
+       a.reaction AS dominant_reaction,
+       ROUND(a.reaction_cnt/ b.total, 2) AS reaction_ratio
+FROM b
+JOIN a
+ON b.user_id = a.user_id
+WHERE a.rn = 1 AND a.reaction_cnt/ b.total > 0.6
+ORDER BY reaction_ratio DESC, b.user_id ASC;
