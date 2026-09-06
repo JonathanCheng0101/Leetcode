ff9bce6f84a797eb69d2996d4dd8d334f23cd4e6
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-06 11:34:55 -0600

    Time: 522 ms (55.65%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,9 +1,9 @@
-# Write your MySQL query statement below
-SELECT DISTINCT t1.id,
-       (CASE WHEN t1.p_id IS NULL THEN 'Root'
-             WHEN t2.p_id IS NULL THEN 'Leaf'
-             ELSE 'Inner'
-        END) AS type
+SELECT t1.id,
+       CASE WHEN t1.p_id IS NULL THEN 'Root'
+            WHEN EXISTS(
+                SELECT 1
+                FROM Tree t2
+                WHERE t2.p_id = t1.id
+            ) THEN 'Inner'
+            ELSE 'Leaf' END AS type
 FROM Tree t1
-LEFT JOIN Tree t2
-ON t1.id = t2.p_id
