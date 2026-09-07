49ad5e7d9d2b55495244a6a552cf2622e08d51de
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-07 14:03:24 -0600

    Time: 0 ms (100.00%) | Memory: 19.8 MB (53.56%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -3,9 +3,12 @@ class Solution:
         slow = 0
 
         for fast in range(len(nums)):
-            if nums[fast] % 2 == 0: # even number
+            if nums[fast] % 2 == 0:
                 nums[slow], nums[fast] = nums[fast], nums[slow]
-
+                
                 slow += 1
-
+            
+        
         return nums
+
+       
