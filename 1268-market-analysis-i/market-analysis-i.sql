cbac1739601685d68b3a6252ef7f37016e762582
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-07 10:40:22 -0600

    Time: 1585 ms (25.52%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,13 +1,7 @@
-WITH t AS(
-    SELECT buyer_id,
-       COUNT(buyer_id) AS cnt
-    FROM Orders
-    WHERE order_date >= '2019-01-01' AND order_date < '2020-01-01'
-    GROUP BY buyer_id
-)
 SELECT u.user_id AS buyer_id,
        u.join_date,
-       IFNULL(t.cnt, 0) AS orders_in_2019
+       SUM(CASE WHEN YEAR(o.order_date)= 2019 THEN 1 ELSE 0 END) AS orders_in_2019 
 FROM Users u
-LEFT JOIN t
-ON u.user_id = t.buyer_id;
+LEFT JOIN Orders o
+ON u.user_id = o.buyer_id 
+GROUP BY u.user_id
