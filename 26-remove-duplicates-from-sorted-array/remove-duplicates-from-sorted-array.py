b84ffaa49b28520fbc36885b9bc35e9126731f2a
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-07 13:48:07 -0600

    Time: 3 ms (44.37%) | Memory: 20.7 MB (17.32%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,10 +1,13 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        slow = 0
-        for fast in range(len(nums)):
-            if nums[fast] != nums[slow]:
-                nums[slow + 1] = nums[fast]
+        # fast：掃描員，負責把每個原始元素看過一遍。
+        # slow：寫入員，指向「下一個應該保留元素要放的位置」
 
+        slow = 1
+
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow - 1]:
+                nums[slow] = nums[fast]
                 slow += 1
         
-        return slow+ 1
+        return slow
