480244c81d2efe8e844be3ffcbc1bc861727a385
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-07 11:31:29 -0600

    Time: 1140 ms (6.36%) | Memory: 0B (100.00%) - LeetSync

--- 1198-unpopular-books/unpopular-books.sql
+++ 1198-unpopular-books/unpopular-books.sql
@@ -1,19 +1,21 @@
 WITH t AS(
-    SELECT book_id, name
+    SELECT book_id,
+        name
     FROM Books
-    WHERE available_from < '2019-05-23'
+    WHERE available_from <= DATE_SUB('2019-06-23', INTERVAL 30 DAY)
 ), a AS(
-    SELECT book_id, SUM(quantity) AS quantity
+    SELECT book_id,
+       SUM(quantity) AS quantity
     FROM Orders
-    WHERE dispatch_date BETWEEN '2018-06-23' AND '2019-06-23'
+    WHERE dispatch_date >= DATE_SUB('2019-06-23', INTERVAL 365 DAY) AND dispatch_date <= '2019-06-23'
     GROUP BY book_id
-    HAVING SUM(quantity) >= 10
+), b AS(
+    SELECT t.book_id, t.name, IFNULL(a.quantity, 0) AS quantity
+    FROM t
+    LEFT JOIN a
+    ON t.book_id = a.book_id
 )
-SELECT t.book_id, t.name
-FROM t
-WHERE NOT EXISTS(
-    SELECT 1
-    FROM a
-    WHERE a.book_id = t.book_id
-)
-
+SELECT book_id,
+       name
+FROM b
+WHERE quantity < 10;
