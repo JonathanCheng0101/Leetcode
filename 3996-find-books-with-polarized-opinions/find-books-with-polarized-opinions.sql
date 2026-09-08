5dccfb27002c68c175c6e4b4da96bc6a813951b7
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-08 11:32:59 -0600

    Time: 746 ms (23.93%) | Memory: 0B (100.00%) - LeetSync

--- 3996-find-books-with-polarized-opinions/find-books-with-polarized-opinions.sql
+++ 3996-find-books-with-polarized-opinions/find-books-with-polarized-opinions.sql
@@ -1,22 +1,27 @@
 # Write your MySQL query statement below
 WITH t AS(
-    SELECT *,
-       CASE WHEN session_rating >=4 OR session_rating <= 2 THEN 1 ELSE 0 END AS polarized,
-       MIN(session_rating)OVER(PARTITION BY book_id) AS min_rating,
-       MAX(session_rating)OVER(PARTITION BY book_id) AS max_rating,
-       COUNT(book_id)OVER(PARTITION BY book_id)AS cnt
-    FROM reading_sessions
-),a AS(
-    SELECT book_id, 
-       MAX(max_rating  - min_rating) AS rating_spread,
-       SUM(polarized)/COUNT(book_id) AS polarization_score 
+        SELECT book_id,
+        session_rating,
+        COUNT(book_id) OVER(PARTITION BY book_id) AS cnt,
+        CASE WHEN session_rating >= 4 OR session_rating <= 2 THEN 1 ELSE 0 END AS extreme_chk
+    FROM reading_sessions 
+), a AS(
+    SELECT book_id,
+       MAX(session_rating) - MIN(session_rating) AS rating_spread,
+       SUM(extreme_chk)/ COUNT(book_id) AS polarization_score
     FROM t
-    WHERE cnt >= 5 AND min_rating <= 2 AND max_rating >= 4
+    WHERE cnt >= 5 
     GROUP BY book_id
+    HAVING SUM(extreme_chk)/ COUNT(book_id) >= 0.6 AND MAX(session_rating) >= 4 AND MIN(session_rating) <= 2
 )
-SELECT  a.book_id, b.title, b.author, b.genre, b.pages, a.rating_spread, ROUND(a.polarization_score, 2) AS polarization_score
-FROM a
-JOIN books b
-ON a.book_id = b.book_id
-WHERE rating_spread >= 2 AND polarization_score >= 0.6
-ORDER BY a.polarization_score DESC, b.title DESC;
+SELECT b.book_id,
+       b.title,
+       b.author,
+       b.genre,
+       b.pages,
+       a.rating_spread,
+       ROUND(a.polarization_score, 2) AS polarization_score
+FROM books b
+JOIN a
+ON b.book_id = a.book_id
+ORDER BY polarization_score DESC, b.title DESC;
