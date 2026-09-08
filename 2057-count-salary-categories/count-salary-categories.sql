42f4a9d646548e5907a2bdf5616686037593be95
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-08 10:39:34 -0600

    Time: 1711 ms (31.48%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,23 +1,21 @@
-WITH t AS (
+
+WITH t AS(
+    SELECT account_id,
+        income,
+        CASE WHEN income < 20000 THEN 'Low Salary'
+                WHEN income > 50000 THEN 'High Salary'
+                ELSE 'Average Salary' END AS category
+    FROM Accounts
+), a AS(
     SELECT 'Low Salary' AS category
     UNION ALL
     SELECT 'Average Salary' AS category
     UNION ALL
     SELECT 'High Salary' AS category
+
 )
-, a AS(
-    SELECT *,
-       CASE WHEN income < 20000 THEN 'Low Salary'
-            WHEN income > 50000 THEN 'High Salary'
-            ELSE 'Average Salary' END AS status
-    FROM Accounts
-), b AS(
-    SELECT status, COUNT(status) AS accounts_count
-    FROM a
-    GROUP BY status
-)
-SELECT t.category,
-       IFNULL(b.accounts_count, 0) AS accounts_count
-FROM t
-LEFT JOIN b
-ON t.category = b.status;
+SELECT a.category, IFNULL(COUNT(t.category), 0) AS accounts_count
+FROM a
+LEFT JOIN t
+ON a.category = t.category
+GROUP BY t.category
