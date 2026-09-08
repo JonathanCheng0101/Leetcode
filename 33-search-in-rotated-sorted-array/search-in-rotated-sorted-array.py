3b12ae3ad06ffbf1c7c258e8e13ce9cdac40e13b
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-08 14:38:23 -0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (96.03%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -4,22 +4,21 @@ class Solution:
 
         while l <= r:
             mid = (l + r) // 2
+
             if nums[mid] == target:
                 return mid
-
-            # 找左右邊照順序
+            
             elif nums[l] <= nums[mid]:
-                # 左邊照順序 找左邊
                 if nums[l] <= target < nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
 
-            elif nums[mid] <= nums[r]:
-                # 右邊照順序 找右邊
+            elif nums[mid] < nums[r]:
                 if nums[mid] < target <= nums[r]:
                     l = mid + 1
                 else:
                     r = mid - 1
-
+        
         return -1
+       
