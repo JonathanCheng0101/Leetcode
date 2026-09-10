eace2b76532a8f53712265000dcab40c678caad8
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-10 11:36:16 -0600

    Time: 61 ms (35.49%) | Memory: 19.6 MB (43.70%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,26 +1,24 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def isPalindrome(s):
+        def isPalindrom(s):
             l, r = 0, len(s) - 1
-            while l < r:
+            while l <= r:
                 if s[l] != s[r]:
                     return False
                 l += 1
                 r -= 1
             return True
+        
 
         l, r = 0, len(s) - 1
-        while l < r:
+        while l <= r:
             if s[l] != s[r]:
-                return isPalindrome(s[l:r]) or isPalindrome(s[l + 1:r + 1])
-            
-            l += 1
-            r -= 1
-        
-        return True
-        
-            
+                return isPalindrom(s[l:r]) or isPalindrom(s[l + 1:r + 1])
 
+            else:
+                l += 1
+                r -= 1
             
-    
+        return True
+
         
