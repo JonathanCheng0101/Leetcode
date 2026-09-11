f4d06214b8471f8fdd2889718bf946303e47bb89
JonathanCheng0101 <77355451+JonathanCheng0101@users.noreply.github.com>
2026-09-11 11:58:57 -0600

    Time: 1117 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,15 +1,13 @@
-# Write your MySQL query statement below
 WITH t AS(
-    SELECT visited_on,
-        SUM(amount) AS amount
+    SELECT visited_on, SUM(amount) AS amount
     FROM Customer
     GROUP BY visited_on
-), b AS(
+), a AS(
     SELECT visited_on,
-       SUM(amount)OVER(ORDER BY visited_on ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
-       ROUND(SUM(amount)OVER(ORDER BY visited_on ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)/ 7, 2) AS average_amount
+        SUM(amount)OVER(ORDER BY visited_on ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+        ROUND(SUM(amount)OVER(ORDER BY visited_on ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)/ 7, 2) AS average_amount
     FROM t
 )
 SELECT *
-FROM b
+FROM a
 WHERE visited_on >= DATE_ADD((SELECT MIN(visited_on) FROM Customer), INTERVAL 6 DAY)
